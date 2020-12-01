06010ad2a460d0f01c959dd9a9549a1502064b68
agimz0330 <50951094+agimz0330@users.noreply.github.com>
2020-12-02 03:34:18 +0800

    a little

---
+++ js/publishArticle.js
@@ -0,0 +1,21 @@
+console.log("load");
+document.querySelector("html").classList.add('js');
+
+var fileInput  = document.querySelector( ".input-file" ),  
+    button     = document.querySelector( ".input-file-trigger" ),
+    the_return = document.querySelector( ".file-return");
+    
+button.addEventListener( "keydown", function( event ) {  
+    if ( event.keyCode == 13 || event.keyCode == 32 ) {  
+        fileInput.focus();  
+    }  
+});
+button.addEventListener( "click", function( event ) {
+    fileInput.focus();
+    return false;
+});  
+fileInput.addEventListener( "change", function( event ) { 
+    var str= this.value;
+    str= str.substring(12, str.length);
+    the_return.innerHTML = str;
+});  
