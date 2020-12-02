7607a611a1c45456a2d2d719560a90e0eec8f664
ytchao0234 <nd68319@gmail.com>
2020-12-02 19:46:50 +0800

    Chao update report js file swal alert

--- js/report.js
+++ js/report.js
@@ -1,4 +1,4 @@
-var articles = [ {"ArticleID": "123"} ];
+var articles = [ { "articleID": "123", "reason": "hahaha" }, { "articleID": "456", "reason": "hahaha" }, { "articleID": "789", "reason": "hahaha" } ];
 
 $( document ).ready( function() 
 {
@@ -9,44 +9,130 @@ $( document ).ready( function()
         console.log( $(this).text() );
 
         if( $(this).text() != "檢舉文章列表為空" )
-            location.href =  "../html/post.html" + "?id=" + articles[ $( ".tabContent tr" ).index( this.closest( "tr" ) ) ][ "ArticleID" ];
+        {
+            sessionStorage.setItem( "articleID", articles[ $( ".tabContent tr" ).index( this.closest( "tr" ) ) ][ "articleID" ] );
+            location.href =  "../html/post.html";
+        }
     } );
 
     $( ".tabContent button" ).on( "click", function()
     {
         // console.log( $( ".tabContent tr" ).index( this.closest( "tr" ) ) );
 
-        if( $(this).text().trim() == "刪除" )
+        if( $(this).text().trim() == "原因" )
         {
-            console.log( "delete" );
+            console.log( "reason" );
+            // alert( articles[ $( ".tabContent tr" ).index( this.closest( "tr" ) ) ][ "reason" ] );
+            
+            swal({
+                title: "檢舉原因<br /><small>&lt;" + $(this).closest( "td" ).prev().text() + "&gt;</small>",
+                text: articles[ $( ".tabContent tr" ).index( this.closest( "tr" ) ) ][ "reason" ],
+                animation: false
+            })
+        }
+        else if( $(this).text().trim() == "刪除" )
+        {
+            // let cmd = {};
+            // cmd[ "act" ] = "deleteArticle";
+            // cmd[ "articleID" ] = articles[ $( ".tabContent tr" ).index( this.closest( "tr" ) ) ][ "articleID" ];
 
-            let cmd = {};
-            cmd[ "Act" ] = "DeleteArticle";
-            cmd[ "ArticleID" ] = articles[ $( ".tabContent tr" ).index( this.closest( "tr" ) ) ][ "ArticleID" ];
+            // $.post( ".php", cmd, function( dataDB ){
+            //     dataDB = JSON.parse( dataDB );
 
-            // $.post( ".php", cmd, function( data ){
-            //     data = JSON.parse( data );
-
-            //     if( data.Status == false )
-            //     {
-            //         alert( data.ErrorCode );
-            //     }
-            //     else
-            //     {
-            //         alert( "已成功刪除此篇文章[ " + $(this).closest( "td" ).prev().text() + " ]" );
-            //     }
+            //     swal({
+            //         title: "確定要刪除此篇文章嗎？<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().text() + "&gt;</small>",
+            //         showCancelButton: true,
+            //         confirmButtonText: "確定",
+            //         cancelButtonText: "取消",
+            //         animation: false
+    
+            //         }).then(( result ) => {
+            //             if ( result ) 
+            //             {
+            //                 console.log( "status " + status );
+            //                 if( dataDB.status == false )
+            //                 {
+            //                     swal({
+            //                         title: "刪除失敗<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().text() + "&gt;</small>",
+            //                         type: "error",
+            //                         text: dataDB.errorCode,
+            //                         animation: false
+            //                     })
+            //                 }
+            //                 else
+            //                 {
+            //                     swal({
+            //                         title: "已成功刪除文章！<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().text() + "&gt;</small>",
+            //                         type: "success",
+            //                     })
+            //                     $(this).closest( "tr" ).remove();
+            //                 }
+            //                 $(this).closest( "tr" ).remove();
+            //             }
+            //     }, function( dismiss ) {
+            //         if ( dismiss === 'cancel' );
+            //     });
             // });
 
-            alert( "已成功刪除[ " + $(this).closest( "td" ).prev().text() + " ]" );
+            console.log( "delete" );
+            let status = true;
+            swal({
+                title: "確定要刪除此篇文章嗎？<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().text() + "&gt;</small>",
+                showCancelButton: true,
+                confirmButtonText: "確定",
+                cancelButtonText: "取消",
+                animation: false
+
+                }).then(( result ) => {
+                    if ( result ) 
+                    {
+                        console.log( "status " + status );
+                        if( status == false )
+                        {
+                            swal({
+                                title: "刪除失敗<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().text() + "&gt;</small>",
+                                type: "error",
+                                text: dataDB.errorCode,
+                                animation: false
+                            })
+                        }
+                        else
+                        {
+                            swal({
+                                title: "已成功刪除文章！<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().text() + "&gt;</small>",
+                                type: "success",
+                            })
+                            $(this).closest( "tr" ).remove();
+                        }
+                        $(this).closest( "tr" ).remove();
+                    }
+            }, function( dismiss ) {
+                if ( dismiss === 'cancel' );
+            });
         }
         else if( $(this).text().trim() == "取消" )
         {
             console.log( "cancel" );
+            swal({
+                title: "確定要取消檢舉此篇文章嗎？<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().prev().text() + "&gt;</small>",
+                showCancelButton: true,
+                confirmButtonText: "確定",
+                cancelButtonText: "取消",
+                animation: false
 
-            alert( "已取消檢舉此文章[ " + $(this).closest( "td" ).prev().prev().text() + " ]" );
+                }).then(( result ) => {
+                    if ( result ) 
+                    {
+                        swal({
+                            title: "已成功取消檢舉文章！<br /><small>&lt;" + $(this).closest( "td" ).prev().prev().prev().text() + "&gt;</small>",
+                            type: "success",
+                        })
+                        $(this).closest( "tr" ).remove();
+                    }
+            }, function( dismiss ) {
+                if ( dismiss === 'cancel' );
+            });
         }
-
-        $(this).closest( "tr" ).remove();
     } );
 });
 
@@ -56,77 +142,87 @@ function initial()
     if( !isValid ) return;
 
     let cmd = {};
-    cmd[ "Act" ] = "ReportPage";
-    cmd[ "BoardName" ] = getUrlParameter( "BoardName" );
+    cmd[ "act" ] = "reportPage";
+    cmd[ "boardName" ] = sessionStorage.getItem( "boardName" );
 
-    // $.post( ".php", cmd, function( data )
+    // $.post( "../php/report.php", cmd, function( dataDB )
     // {
-    //     data = JSON.parse( data );
+    //     dataDB = JSON.parse( dataDB );
 
-    //     if( data.status == false )
+    //     if( dataDB.status == false )
     //     {
-    //         alert( data.ErrorCode );
+    //         alert( dataDB.errorCode );
     //     }
     //     else
     //     {
-    //         let content = $( ".tabContent tbody" );
-    //         content.empty();
-
-    //         let empty = true;
-    //         for( let i in data.Data )
-    //         {
-    //             empty = false;
-    //             articles.push( data.Data[i] );
-
-    //             let oneRow = "<tr>" + 
-    //                             "<td>" + data.Data[i][ "Title" ] + "</td>" +
-    //                             "<td>" +
-    //                                 "<button type='button' class='btn'>" +
-    //                                     "<span class='glyphicon glyphicon-trash'></span> 刪除" +
-    //                                 "</button>" +
-    //                             "</td>" +
-    //                             "<td>" +
-    //                                 "<button type='button' class='btn btn-default'>" +
-    //                                     "<span class='glyphicon glyphicon-remove'></span> 取消" +
-    //                                 "</button>" +
-    //                             "</td>" +
-    //                          "</tr>";
-
-    //             content.append( oneRow );
-    //         }
-
-    //         if( empty )
-    //         {
-    //             let emptyMessage = "<tr>" + 
-    //                                     "<td colspan='3'>檢舉文章列表為空</td>" +
+    //             let content = $( ".tabContent tbody" );
+    //             content.empty();
+            
+    //             let empty = true;
+    //             for( let i in dataDB.data )
+    //             {
+    //                 empty = false;
+    //                 articles.push( dataDB.data[i] );
+            
+    //                 let oneRow = "<tr>" + 
+    //                                 "<td>" + dataDB.data[i][ "Title" ] + "</td>" +
+    //                                 "<td>" +
+    //                                     "<button type='button' class='btn btn-default'>" +
+    //                                         "<span class='glyphicon glyphicon-book'></span> 原因" +
+    //                                     "</button>" +
+    //                                 "</td>" +
+    //                                 "<td>" +
+    //                                     "<button type='button' class='btn'>" +
+    //                                         "<span class='glyphicon glyphicon-trash'></span> 刪除" +
+    //                                     "</button>" +
+    //                                 "</td>" +
+    //                                 "<td>" +
+    //                                     "<button type='button' class='btn'>" +
+    //                                         "<span class='glyphicon glyphicon-remove'></span> 取消" +
+    //                                     "</button>" +
+    //                                 "</td>" +
     //                                 "</tr>";
-    //             content.append( emptyMessage );
-    //         }
+            
+    //                 content.append( oneRow );
+    //             }
+            
+    //             if( empty )
+    //             {
+    //                 let emptyMessage = "<tr>" + 
+    //                                         "<td colspan='4'>檢舉文章列表為空</td>" +
+    //                                     "</tr>";
+    //                 content.append( emptyMessage );
+    //             }
     //     }
     // } );
 
     let content = $( ".tabContent tbody" );
     content.empty();
 
-    let data = {};
-    data[ "Data" ] = [ { "Title": "紅燈區" }, { "Title": "大一妹妹看起來很波霸哦"}, { "Title": "看我切開兔子的肚皮" } ];
-    // data[ "Data" ] = [];
+    let dataDB = {};
+    dataDB[ "reason" ] = "hahaha";
+    dataDB[ "data" ] = [ { "Title": "紅燈區" }, { "Title": "大一妹妹看起來很波霸哦" }, { "Title": "看我切開兔子的肚皮"} ];
 
     let empty = true;
-    for( let i in data.Data )
+    for( let i in dataDB.data )
     {
         empty = false;
-        articles.push( data.Data[i] );
+        articles.push( dataDB.data[i] );
 
         let oneRow = "<tr>" + 
-                        "<td>" + data.Data[i][ "Title" ] + "</td>" +
+                        "<td>" + dataDB.data[i][ "Title" ] + "</td>" +
                         "<td>" +
-                            "<button type='button' class='btn'>" +
+                            "<button type='button' class='btn btn-default'>" +
+                                "<span class='glyphicon glyphicon-book'></span> 原因" +
+                            "</button>" +
+                        "</td>" +
+                        "<td>" +
+                            "<button type='button' class='btn btn-danger'>" +
                                 "<span class='glyphicon glyphicon-trash'></span> 刪除" +
                             "</button>" +
                         "</td>" +
                         "<td>" +
-                            "<button type='button' class='btn btn-default'>" +
+                            "<button type='button' class='btn'>" +
                                 "<span class='glyphicon glyphicon-remove'></span> 取消" +
                             "</button>" +
                         "</td>" +
@@ -138,7 +234,7 @@ function initial()
     if( empty )
     {
         let emptyMessage = "<tr>" + 
-                                "<td colspan='3'>檢舉文章列表為空</td>" +
+                                "<td colspan='4'>檢舉文章列表為空</td>" +
                             "</tr>";
         content.append( emptyMessage );
     }
@@ -146,75 +242,11 @@ function initial()
 
 function checkPermission()
 {
-    let cmd = {};
-    cmd[ "Act" ] = "CheckPermission";
-    cmd[ "UserID" ] = getCookie( "UserID" );
-    cmd[ "Password" ] = getCookie( "Password" );
+    // let perm = sessionStorage.getItem( "permission" );
+    // console.log( perm );
 
-    let isValid = true;
+    // if( perm ) return ( perm.valueOf() >= 2 ); 
+    // else return false;
 
-    // $.post( ".php", cmd, function( data )
-    // {
-    //     data = JSON.parse( data );
-
-    //     if( data.status == true && data.Data.Permission >= 3 )
-    //     {
-    //         isValid = true;
-    //     }
-    //     else
-    //     {
-    //         if( data.status == false )
-    //         {
-    //             alert( "ErrorCode" );
-    //         }
-    //         else if( data.Data.Permission < 2 )
-    //         {
-    //             alert( "Permission Denied" );
-    //         }
-
-    //         $( document ).empty();
-    //     }
-    // });
-
-    return isValid;
+    return true;
 }
-
-var getUrlParameter = function getUrlParameter(sParam) {
-    let sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-};
-
-function setCookie(cname, cvalue, exdays) {
-    let d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-} 
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-
-    for( let i in ca) {
-        let c = ca[i];
-
-        while ( c.charAt(0) == ' ' ) {
-            c = c.substring(1);
-         }
-         if ( c.indexOf( name ) == 0 ) {
-            return c.substring( name.length, c.length );
-         }
-     }
-    return "";
-} 
