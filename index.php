798ddbdedbe2d29a24861ed67a847be93d84ff38
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-02 23:50:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+    header("Content-Type:text/html; charset=utf-8");
+    date_default_timezone_set("Asia/Taipei");
+	error_reporting(1);
+	error_reporting(E_ALL);
+    $input = json_decode(file_get_contents('php://input', 'r'), true); 
+    require_once("connectDB.php");
+	$conn = connSql();
+	switch($input["act"]){
+        case "logIn": //登入
+	        require_once("logIn.php");
+	        doLogIn($input);
+	        break;
+        case "creatAccount": //註冊
+            require_once("signUp.php");
+	        doCreatAccount($input);
+            break;
+        // case "forgottenPwd": //忘記密碼
+        //     forgetPassword.php;
+        //     break;
+        // case "home": // 系統首頁
+        //     home.php
+        //     break;
+        // case "publishArticle"://新增文章
+        //     publishArticle.php
+        // case "report":	//檢視檢舉
+        //     admin.php
+        //     break;
+    }
+?>
