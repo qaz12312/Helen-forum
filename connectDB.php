ad08c8caeb1c81539ff606e569326ebfaa1358ee
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-02 22:25:24 +0800

    finish

--- connectDB.php
+++ connectDB.php
@@ -3,7 +3,7 @@
         $serverName = "helendb.chw8qvtxo3mu.us-east-1.rds.amazonaws.com";
         $userName = "helen";
         $password = "ynVdCeYKBrDJ";
-        $databaseName = "helendb";
+        $databaseName = "test";
         $conn = mysqli_connect($serverName, $userName, $password, $databaseName);
         mysqli_query($conn, 'SET NAMES utf8');
         if($conn->connect_error){
