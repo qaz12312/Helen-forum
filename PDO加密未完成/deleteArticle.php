8aa006d783d19586c214fbe2c8a51273c885dce7
PollyYang <watermoo22@gmail.com>
2020-12-08 00:31:16 +0800

    Update deleteArticle.php

--- PDO加密未完成/deleteArticle.php
+++ PDO加密未完成/deleteArticle.php
@@ -11,9 +11,9 @@
     dataDB.status
     若 status = true:
         dataDB.errorCode = ""    
-        dataDB.data = ""
+        dataDB.data = "成功刪除此文章"
     否則
-        dataDB.errorCode = ""
+        dataDB.errorCode = "You don't have permission to do this action." / "查詢失敗，無此文章" / "刪除失敗，資料庫異常"
         dataDB.data = ""
     */
     function doDeleteArticle($input){
@@ -34,30 +34,31 @@
             if($result->num_rows <= 0){
                 $rtn = array();
                 $rtn["status"] = false;
-                $rtn["errorCode"] = "無權限刪除";
+                $rtn["errorCode"] = "查詢失敗，無此文章";
                 $rtn["data"] = "";
             }
             else{
-                $del="DELETE FROM `Article` WHERE `ArticleID` = '".$input['articleID']."' AND  `AuthorID` = '".$input['account']."'";
+                $del="DELETE FROM `Article` WHERE `ArticleID` = '".$input['articleID']."' AND  `AuthorID` = '".$userInfo['account']."'";
                 $result=$conn->query($del);
                 if(!$result){
                     die($conn->error);
                 }
-                $sql="SELECT `ArticleID` FROM `Article` WHERE `ArticleID` = '".$input['articleID']."' ";
+                $sql="SELECT `ArticleID` FROM `Article` WHERE `ArticleID` = '".$input['articleID']."'";
                 $result=$conn->query($sql);
                 if(!$result){
                     die($conn->error);
                 }
                 if($result->num_rows > 0){
-                $rtn = array();
-                $rtn["status"] = false;
-                $rtn["errorCode"] = "刪除失敗，資料庫異常";
-                $rtn["data"] = "";
+                    $rtn = array();
+                    $rtn["status"] = false;
+                    $rtn["errorCode"] = "刪除失敗，資料庫異常";
+                    $rtn["data"] = "";
                 }
                 else{
                     $rtn = array();
                     $rtn["status"] = true;
                     $rtn["errorCode"] = "";
+                    $rtn["data"] = "成功刪除此文章";
                 }
             }
             echo json_encode($rtn);
