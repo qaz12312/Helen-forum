73b1e9449e66c372fca1caf6587c5be8d226e553
agimz0330 <50951094+agimz0330@users.noreply.github.com>
2020-12-08 01:26:48 +0800

    11222

---
+++ js/bar.js
@@ -0,0 +1,25 @@
+let boards= [];// boardName
+let userData= {};
+
+$(document).ready(function(){
+    initial();
+})
+
+function initial(){
+    let dataDB= {}
+    // $.post( "../index.php", cmd, function(dataDB){
+    //     dataDB = JSON.parse( dataDB );
+
+    //     if( dataDB.status == false )
+    //     {
+    //         swal({
+    //             title: "載入頁面失敗",
+    //             type: "error",
+    //             text: dataDB.errorCode
+    //         })
+    //     }
+        // else{
+            s
+        // }
+    // });
+}
