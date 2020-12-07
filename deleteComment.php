271d11cb9953a49533a2dc8b92014ccf2cb80797
PollyYang <watermoo22@gmail.com>
2020-12-08 02:07:58 +0800

    updata errorCode+data

--- deleteComment.php
+++ deleteComment.php
@@ -9,13 +9,14 @@
 
     後端 to 前端
     dataDB.status
-    dataDB.errorCode
     若 status = true:
-        
+        dataDB.errorCode = ""
+        dataDB.data= "成功刪除此留言"
     否則
+        dataDB.errorCode = "You don't have permission to do this action." / "此留言不存在" / "刪除失敗，資料庫異常"
         dataDB.data = ""
     */
-    $sqlcheck="SELECT `ArticleID` FROM `Comments` NATURAL JOIN`Users`  WHERE `ArticleID`='".$input['articleID']."' AND `AuthorID`='".$input['account']."' AND`Floor`='".$input['floors']."'";  
+    $sqlcheck="SELECT `ArticleID` FROM `Comments` NATURAL JOIN `Users`  WHERE `ArticleID`='".$input['articleID']."' AND `AuthorID`='".$input['account']."' AND`Floor`='".$input['floors']."'";  
     $result=$conn->query($sqlcheck);
     if(!$result){
         die($conn->error);
@@ -23,7 +24,7 @@
     if($result->num_rows <= 0){
         $rtn = array();
         $rtn["status"] = false;
-        $rtn["errorCode"] = "無權限刪除";
+        $rtn["errorCode"] = "此留言不存在";
         $rtn["data"] = "";
     }
     else{    
@@ -40,13 +41,13 @@
         if($result->num_rows > 0){
             $rtn = array();
             $rtn["status"] = false;
-            $rtn["errorCode"] = "留言刪除失敗";
+            $rtn["errorCode"] = "刪除失敗，資料庫異常";
             $rtn["data"] = "";
         }
         else{
             $rtn = array();
             $rtn["status"] = true;
-            $rtn["errorCode"] = "";
+            $rtn["errorCode"] = "成功刪除此留言";
         }
     }
     echo json_encode($rtn);
