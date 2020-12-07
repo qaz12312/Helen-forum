6bc7f22e9172d13f053adfb9e6f11295c2e90e49
PollyYang <watermoo22@gmail.com>
2020-12-07 18:28:09 +0800

    ok: test all

--- browseAuthority.php
+++ browseAuthority.php
@@ -9,45 +9,48 @@
 	dataDB.status
 	若 status = true:
 		dataDB.errorCode = ""
-		dataDB.data[0] // UserID:"00857210@mail.ntou.edu.tw"
-		dataDB.data[1] // Permissions:1
-		dataDB.data[2] // Color:"#ffffff"
-		dataDB.data[3] // Nickname:"cook"
+		dataDB.data[0] // Permissions:1
+		dataDB.data[1] // BoardName
+		(
+            dataDB.data[1][0]
+            .....
+        )
 	否則
-		dataDB.errorCode = "This account(00857210@mail.ntou.edu.tw) does not exist" /  "Incorrect account or password"
+		dataDB.errorCode = "This account(00857210@mail.ntou.edu.tw) does not exist" 
 		dataDB.data = "" 
 	*/
-    function browseAuthority($input){
-    	global $conn;
-    	$sql="SELECT `UserID`,`Permissions`,`Color`,`Nickname` FROM `Users` WHERE `UserID`='".$input['account']."' AND `Password`='".$input['password']."'";
+    function doBrowseAuthority($input){
+        global $conn;
+    	$sql="SELECT `Permissions` FROM `Users` WHERE `UserID`='".$input['account']."'";
 	    $result = $conn->query($sql);
 	    if(!$result){
 	        die($conn->error);
 	    }
 	    if($result->num_rows <= 0){
-			$sql="SELECT `UserID`,`Permissions`,`Color`,`Nickname` FROM `Users` WHERE `UserID`='".$input['account']."'";
-			$resultFindAccount = $conn->query($sql);
-			if(!$resultFindAccount){
-				die($conn->error);
-			}
-			if($resultFindAccount->num_rows <= 0){
-				$rtn = array();
-				$rtn["status"] = false;
-				$rtn["errorCode"] = "The account(".$input['account'].") does not exist";
-				$rtn["data"] = "";
-			}else{
-				$rtn = array();
-				$rtn["status"] = false;
-				$rtn["errorCode"] = "Incorrect account or password";
-				$rtn["data"] = "";
-			}
+            $rtn = array();
+            $rtn["status"] = false;
+            $rtn["errorCode"] = "This account(".$input['account'].") does not exist";
+            $rtn["data"] = "";
 	    }
 	    else{
 	        $row=$result->fetch_row();
 	        $rtn = array();
 	        $rtn["status"] = true;
 	        $rtn["errorCode"] = "";
-	        $rtn["data"] =$row;
+            $rtn["data"][0] =$row[0];
+
+			$sql="SELECT `BoardName` FROM `Board` WHERE `UserID`='".$input['account']."'";
+			$result = $conn->query($sql);
+				if(!$result){
+				die($conn->error);
+			}
+			$row = array();
+			
+			for($i=1;$i<=$result->num_rows;$i++){
+				$row=$result->fetch_row();
+				print_r($row);
+                $rtn["data"][1][$i]=$row[0];
+            }
         }
 		echo json_encode($rtn);
     }
