f02836e01a954046d2fdf0b5d0039bbc502d4dd7
PollyYang <watermoo22@gmail.com>
2020-12-08 02:46:22 +0800

    測試方式已更新~
    更簡單的測試~
    只要改index.php

--- 測試/index.php
+++ 測試/index.php
@@ -16,15 +16,15 @@
 // 改--自己放測試資料 ////////////////////////////////////////////
     $input = array();
 // 改(情況一)--自己放php檔+function ////////////////////////////////////////////
-    // $input["act"] = "logIn";
-	// $input["account"] = "admin";
-    // $input["password"] = "admin";
-    // require_once("logIn.php");
-    // doLogIn($input);
+    $input["act"] = "logIn";
+	$input["account"] = "00757003";
+    $input["password"] = "00757003";
+    require_once("logIn.php");
+    doLogIn($input);
 // 改(情況二)--自己放php檔+function ////////////////////////////////////////////
-    $input["act"] = "modifyPersonalInfo";
-    $input["option"] = /*"password" / */"nickname"/* / "color"*/;
-    $input["account"] = "00757003";
-    $input["new"] = /*"987654321" / */"sewateryyyyy"/* / "\#028d5f"*/;
-    require_once("personalInfo.php");
+    // $input["act"] = "modifyPersonalInfo";
+    // $input["option"] = "password"  /*/ "nickname" / "color"*/;
+    // $input["account"] = "00757003";
+    // $input["new"] = "00757003" /*/ "sewateryyyyy" / "\#028d5f"*/;
+    // require_once("personalInfo.php");
 ?>
