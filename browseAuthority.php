99b6e3dfc1859ac35f4a8e128be88735e9714f0d
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-07 19:27:01 +0800

    Update browseAuthority.php

--- browseAuthority.php
+++ browseAuthority.php
@@ -9,13 +9,13 @@
 	dataDB.status
 	若 status = true:
 		dataDB.errorCode = ""
-		dataDB.data.Permissions // Permissions:1
-		dataDB.data.Color // Color:"#ffffff"
-		dataDB.data.Nickname // NickName:"00857210"
-		dataDB.data.BoardName // BoardName
+		dataDB.data.permissions // Permission:1
+		dataDB.data.color // Color:"#ffffff"
+		dataDB.data.nickname // NickName:"00857210"
+		dataDB.data.boardName // BoardName
 		(
-			dataDB.data.BoardName[0]
-			dataDB.data.BoardName[1]
+			dataDB.data.boardName[0]
+			dataDB.data.boardName[1]
             .....
         )
 	否則
@@ -40,9 +40,9 @@
 	        $rtn = array();
 	        $rtn["status"] = true;
 			$rtn["errorCode"] = "";
-			$rtn["data"]["Permissions"] =$row[0]["Permissions"];
-			$rtn["data"]["Color"] =$row[0]["Color"];
-			$rtn["data"]["Nickname"] =$row[0]["Nickname"];
+			$rtn["data"]["permissions"] =$row[0]["Permissions"];
+			$rtn["data"]["color"] =$row[0]["Color"];
+			$rtn["data"]["nickname"] =$row[0]["Nickname"];
 
 			$sql="SELECT `BoardName` FROM `Board` WHERE `UserID`='".$input['account']."'";
 			$result = $conn->query($sql);
@@ -54,7 +54,7 @@
 			for($i=0;$i<$result->num_rows;$i++){
 				$row=$result->fetch_row();
 				print_r($row);
-                $rtn["data"]["BoardName"][$i]=$row[0];
+                $rtn["data"]["boardName"][$i]=$row[0];
             }
         }
 		echo json_encode($rtn);
