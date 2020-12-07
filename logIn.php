7b8cf4c6002eeedd5af299c7ac89a1fe7b595939
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-07 20:12:22 +0800

    Update logIn.php

--- logIn.php
+++ logIn.php
@@ -3,20 +3,19 @@
 	前端 to 後端:
 	let cmd = {};
 	cmd["act"] = "logIn";
-	cmd["account"] = "00857210@mail.ntou.edu.tw";
-	cmd["password"] = "123456789";
-
+	cmd["account"] = "00757007";
+	cmd["password"] = "00757007";
 	後端 to 前端:
 	dataDB = JSON.parse(data);
 	dataDB.status
 	若 status = true:
 		dataDB.errorCode = ""
-		dataDB.data[0] // UserID:"00857210@mail.ntou.edu.tw"
+		dataDB.data[0] // UserID:"00757007"
 		dataDB.data[1] // Permissions:1
 		dataDB.data[2] // Color:"#ffffff"
-		dataDB.data[3] // Nickname:"cook"
+		dataDB.data[3] // Nickname:"00757007"
 	否則
-		dataDB.errorCode = "找不到會員"
+		dataDB.errorCode = "This account(00757007) does not exist" /  "Incorrect account or password"
 		dataDB.data = "" 
 	*/
     function doLogIn($input){
@@ -27,10 +26,22 @@
 	        die($conn->error);
 	    }
 	    if($result->num_rows <= 0){
-	        $rtn = array();
-	        $rtn["status"] = false;
-	        $rtn["errorCode"] = "找不到會員";
-	        $rtn["data"] = "";
+			$sql="SELECT `UserID`,`Permissions`,`Color`,`Nickname` FROM `Users` WHERE `UserID`='".$input['account']."'";
+			$resultFindAccount = $conn->query($sql);
+			if(!$resultFindAccount){
+				die($conn->error);
+			}
+			if($resultFindAccount->num_rows <= 0){
+				$rtn = array();
+				$rtn["status"] = false;
+				$rtn["errorCode"] = "The account(".$input['account'].") does not exist";
+				$rtn["data"] = "";
+			}else{
+				$rtn = array();
+				$rtn["status"] = false;
+				$rtn["errorCode"] = "Incorrect account or password";
+				$rtn["data"] = "";
+			}
 	    }
 	    else{
 	        $row=$result->fetch_row();
@@ -38,7 +49,7 @@
 	        $rtn["status"] = true;
 	        $rtn["errorCode"] = "";
 	        $rtn["data"] =$row;
-	    }
+        }
 		echo json_encode($rtn);
     }
 ?>
