1757b2bb3dce7e3c6dec6b4a1404e7add8767c5b
PollyYang <watermoo22@gmail.com>
2020-12-07 23:46:50 +0800

    ok

--- PDO加密未完成/personalInfo.php
+++ PDO加密未完成/personalInfo.php
@@ -4,7 +4,7 @@
     let cmd = {};
     cmd["act"] = "modifyPersonalInfo";
     cmd["option"] = "password" / "nickname" / "color";
-    cmd["account"] = "00857210@mail.ntou.edu.tw";
+    cmd["token"] = "c93b3e8ab496d786030fbf8a17c3da51";
     cmd["new"] = "987654321" / "beauty cook" / "\#028d5f";
 
     後端 to 前端:
@@ -14,7 +14,7 @@
         dataDB.errorCode = ""
         dataDB.data = "success to change the (password/nickname/color)"
     否則
-        dataDB.errorCode = "找不到用戶，(密碼/暱稱/頭貼)更改失敗，需重新登入系統" / "資料庫異常，(密碼/暱稱/頭貼)更改失敗，需重新登入系統"
+        dataDB.errorCode = "You don't have permission to do this action." / "找不到用戶，(密碼/暱稱/頭貼)更改失敗，需重新登入系統" / "資料庫異常，(密碼/暱稱/頭貼)更改失敗，需重新登入系統"
         dataDB.data = "" 
 	*/
     switch($input["option"]){
@@ -35,35 +35,44 @@
 ?>
 
 <?php
-    function doChangeInfo($input,$optionChinese,$optionAttr){
-    	global $conn;
-    	$sql="SELECT `UserID` FROM `Users` WHERE `UserID`='".$input['account']."'";
-        $result=$conn->query($sql);
-        if(!$result){
-            die($conn->error);
-        }
-        if($result->num_rows <= 0){
-            $rtn = array();
+    function doChangeInfo($inputJS,$optionChinese,$optionAttr){
+        global $conn;
+        $token =$inputJS['token'];
+        if(!isset($_SESSION[$token])){
+           $rtn = array();
             $rtn["status"] = false;
-            $rtn["errorCode"] = "找不到用戶，".$optionChinese."更改失敗，需重新登入系統";
+            $rtn["errorCode"] = "You don't have permission to do this action.";
             $rtn["data"] = "";
-        }
-        else{
-            $sql="UPDATE `Users` SET `".$optionAttr."`='".$input['new']."' WHERE `UserID` ='".$input['account']."'";
+        }else{
+            $input = $_SESSION[$token];
+            $sql="SELECT `UserID` FROM `Users` WHERE `UserID`='".$input['account']."'";
             $result=$conn->query($sql);
             if(!$result){
+                die($conn->error);
+            }
+            if($result->num_rows <= 0){
                 $rtn = array();
                 $rtn["status"] = false;
-                $rtn["errorCode"] = "資料庫異常，".$optionChinese."更改失敗，需重新登入系統";
+                $rtn["errorCode"] = "找不到用戶，".$optionChinese."更改失敗，需重新登入系統";
                 $rtn["data"] = "";
-                die($conn->error);
-            }else{
-                $rtn = array();
-                $rtn["status"] = true;
-                $rtn["errorCode"] = "";
-                $rtn["data"] = "success to change the ".$input["option"];
             }
+            else{
+                $sql="UPDATE `Users` SET `".$optionAttr."`='".$input['new']."' WHERE `UserID` ='".$input['account']."'";
+                $result=$conn->query($sql);
+                if(!$result){
+                    $rtn = array();
+                    $rtn["status"] = false;
+                    $rtn["errorCode"] = "資料庫異常，".$optionChinese."更改失敗，需重新登入系統";
+                    $rtn["data"] = "";
+                    die($conn->error);
+                }else{
+                    $rtn = array();
+                    $rtn["status"] = true;
+                    $rtn["errorCode"] = "";
+                    $rtn["data"] = "success to change the ".$input["option"];
+                }
+            }
+            echo json_encode($rtn);
         }
-        echo json_encode($rtn);
     }
 ?>
