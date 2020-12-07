2aa014fa9f48c1e150c3d12de91b0d5111d9984b
PollyYang <watermoo22@gmail.com>
2020-12-08 02:49:58 +0800

    將多個php整合成一個function

--- 整合至同個func/changePassword.php
+++ 整合至同個func/changePassword.php
@@ -1,22 +1,19 @@
-<!-- 前端 to 後端:
-let cmd = {};
-cmd["act"] = "changePassword";
-cmd["account"] = "0123456";
-cmd["password"] = "789789789";
-<!--
-後端 to 前端
-dataDB.status
-dataDB.errorCode
-若 status = true:
-dataDB.data="success to change the password"
-否則
-dataDB.data = "fail to change the password" -->
-
-<!--User[UserID,Password,Permission,Color,Nickname]-->
-
 <?php
-	require_once 'connectDB.php'; //連線資料庫
+	/*前端 to 後端:
+	let cmd = {};
+	cmd["act"] = "changePassword";
+	cmd["account"] = "0123456";
+	cmd["password"] = "789789789";
 
+	後端 to 前端
+	dataDB.status
+	dataDB.errorCode
+	若 status = true:
+	dataDB.data="success to change the password"
+	否則
+	dataDB.data = "fail to change the password"*/
+	
+	require_once 'connectDB.php'; //連線資料庫
 	global $input,$conn;
 	$sql="SELECT `UserID` FROM `Users` WHERE `UserID`='".$input['account']."'";
 	$result=$conn->query($sql);
