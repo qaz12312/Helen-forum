8ff2ef1154a80dae650cfbeb2221f45d79c8f5f0
PollyYang <watermoo22@gmail.com>
2020-12-08 00:41:03 +0800

    deleteBoard to old

--- old版/deleteBoard.php
+++ old版/deleteBoard.php
@@ -1,22 +1,22 @@
 <?php
-    require_once 'test.php'; //連線資料庫 
-    /* 前端 to 後端:
-        let cmd = {};
-       cmd["act"] = "deleteBoard";
-       cmd["userID"] = "00757033";
-      cmd["boardName"] =  "BoardName";
+    /* 
+    前端 to 後端:
+    let cmd = {};
+    cmd["act"] = "deleteBoard";
+    cmd["userID"] = "00757033";
+    cmd["boardName"] =  "BoardName";
+
+    後端 to 前端
+    dataDB.status
+    dataDB.errorCode
+    若 status = true:
+        dataDB.data[0]	// BoardName
+        dataDB.data[1]	// UserID
+        dataDB.data[2]	// Rule
+        dataDB.data[3]	// TopArticleID
+    否則
+        dataDB.data = ""
     */
-/* 後端 to 前端
-            dataDB.status
-            dataDB.errorCode
-            若 status = true:
-				dataDB.data[0]	// BoardName
-				dataDB.data[1]	// UserID
-				dataDB.data[2]	// Rule
-				dataDB.data[3]	// TopArticleID
-            否則
-                dataDB.data = ""
-         */
     $sqlcheck="SELECT `BoardName` FROM `Board` NATURAL JOIN `Users`  WHERE `BoardName`='".$input['boardName']."' AND `UserID`='".$input['account']."' ";  
     $result=$conn->query($sqlcheck);
     if(!$result){
