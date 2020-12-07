beca3ad3904449e1d42ce2b38b2d73ab2c4096de
00757033 <lspss95270@gmail.com>
2020-12-07 23:22:00 +0800

    20201207

--- editBoard.php
+++ editBoard.php
@@ -1,12 +1,12 @@
 <?php 
-       require_once 'connectDB.php'; //連線資料庫
+       require_once 'test.php'; //連線資料庫
       /*前端 to 後端:
         let cmd = {};
         cmd["act"] = "editBoard";
-        cmd["boardName"] = "BoardName"
+        cmd["oldboardName"] = "BoardName"
+        cmd["newboardName"] = "BoardName"
         cmd["account"] = "UserID"
         cmd["rule"] = "Rule"
-        cmd["topArticleID"] = "TopArticleID"
     */ 
     /* 後端 to 前端
             dataDB.state
@@ -14,7 +14,6 @@
             若 state = true:
                 dataDB.data[i] //有i筆文章
                 (
-                    dataDB.data[i].BoardID 
                     dataDB.data[i].BoardName 
                     dataDB.data[i].UserID 
                     dataDB.data[i].Rule 
@@ -23,28 +22,40 @@
             否則
                 
          */
-    $updateSql="UPDATE `Board` SET `BoardName`='".$input['boardName']."'','`Rule`='".$input['rule']."'','`TopArticleID`='".$input['topArticleID']."'";
-    $result=$conn->query($updateSql);
+    $sqlcheck="SELECT `BoardName` FROM `Board` WHERE `BoardName`='".$input['oldboardName']."' AND `UserID`='".$input['account']."' ";  
+    $result=$conn->query($sqlcheck);
     if(!$result){
         die($conn->error);
-    }
-    $sql ="SELECT `BoardName` FROM `Board` NATURAL JOIN`User` ON User.UserID =Board.UserID WHERE `BoardID`='".$input['boardID']."' AND`BoardName`='".$input['boardName']."'AND`Rule`='".$input['rule']."'AND`TopArticleID`='".$input['topArticleID']."'" ;
-    global $conn;
-    $result=$conn->query($sql);
-    if(!$result){
-        die($conn->error);
-    }
+    } 
     if($result->num_rows <= 0){
         $rtn = array();
         $rtn["status"] = false;
-        $rtn["errorCode"] = "沒有版";
+        $rtn["errorCode"] = "無權限刪除";
         $rtn["data"] = "";
     }
     else{
-        $rtn = array();
-        $rtn["status"] = true;
-        $rtn["errorCode"] = "";
-        $rtn["data"] = "";
+        $updateSql="UPDATE `Board` SET `BoardName`='".$input['newboardName']."',`Rule`='".$input['rule']."'where `BoardName`='".$input['oldboardName']."'";
+        $result=$conn->query($updateSql);
+        if(!$result){
+            die($conn->error);
+        }
+        $sql ="SELECT `BoardName` FROM `Board` NATURAL JOIN`Users` WHERE `BoardName`='".$input['newboardName']."'AND`Rule`='".$input['rule']."'AND`UserID`='".$input['account']."'" ;
+        $result=$conn->query($sql);
+        if(!$result){
+            die($conn->error);
+        }
+        if($result->num_rows <= 0){
+            $rtn = array();
+            $rtn["status"] = false;
+            $rtn["errorCode"] = "沒有版";
+            $rtn["data"] = "";
+        }
+        else{
+            $rtn = array();
+            $rtn["status"] = true;
+            $rtn["errorCode"] = "";
+            $rtn["data"] = "";
+        }
     }
     echo json_encode($rtn);
 ?>
