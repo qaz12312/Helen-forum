24120dcca051e7182beff34fc1fc41c08dfe9c11
PollyYang <watermoo22@gmail.com>
2020-12-08 00:05:57 +0800

    Update changePermission.php

--- changePermission.php
+++ changePermission.php
@@ -1,32 +1,30 @@
 <?php
+    /* 
+    前端 to 後端:
+    let cmd = {};
+    cmd["act"] = "EditPermission";
+    cmd["UserID"] = "00757000"
+    cmd["Permission"] = "1"
+    
+    
+    後端 to 前端
+    dataDB.status
+    dataDB.errorCode
+    若 status = true:
+        dataDB.data[0]	// 
+        dataDB.data[1]	// BoardName
+        dataDB.data[2]	// UserID
+    否則
+        dataDB.data = ""
+    */
     require_once 'noSecurity.php'; //連線資料庫 
-
-	function EditPermission()
-	{
-        /* 前端 to 後端:
-            let cmd = {};
-            cmd["act"] = "EditPermission";
-			cmd["UserID"] = "00757000"
-			cmd["Permission"] = "1"
-        */
-		
-        /* 後端 to 前端
-            dataDB.status
-            dataDB.errorCode
-            若 status = true:
-                dataDB.data[0]	// 
-				dataDB.data[1]	// BoardName
-				dataDB.data[2]	// UserID
-            否則
-                dataDB.data = ""
-         */
+	function EditPermission(){
         global $input,$conn;	// 置頂
 		$sql="UPDATE `Users` SET `Permission` = CASE WHEN `Permission` = 1 THEN `Permission` = 2 WHEN `Permission` = 2 THEN `Permission` = 1";
         $result=$conn->query($sql);
         if(!$result){
             die($conn->error);
         }
-
         if($result->num_rows > 0){
             $rtn = array();
             $rtn["status"] = false;
