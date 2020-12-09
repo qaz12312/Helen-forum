4535c2946259f357917487b1d4468c403a9a6eff
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2020-12-09 20:25:54 +0800

    email

--- forgetPassword.php
+++ forgetPassword.php
@@ -1,82 +1,145 @@
-//
-<!-- 前端 to 後端:
-let cmd = {};
-cmd["act"] = "forgetpassword";
-cmd["account"] = 00757003@email.ntou.edu.tw;
--->
-<!-- 後端 to 前端
-dataDB.status
-dataDB.errorCode
-若 status = true:
-dataDB.data = "系統已向您的郵箱傳送了一封郵件<br/>請登入到您的郵箱及時重置您的密碼！"
-否則
-dataDB.data = "尚未註冊" -->
-//
-<!-- 前端 to 後端:
-let cmd = {};
-cmd["act"] = "input email token";
-cmd["account"] = 00757003@email.ntou.edu.tw;
-cmd["token"] = 一組驗證碼;
--->
-<!-- 後端 to 前端
-dataDB.status
-dataDB.errorCode
-若 status = true:
-dataDB.data = ""
-否則
-dataDB.data = "" -->
-<?php
-	require_once("connectDB.php");//連線資料庫 
-	$email = stripslashes(trim($_POST['account'])); 
-	$sql = "select `UserID`, `Password` from `User` where `UserID`='$email'"; 
-	$query = mysql_query($sql); 
-	$num = mysql_num_rows($query); 
-	if($num==0){	//該郵箱尚未註冊！ 
-		$rtn = array();
-		$rtn["status"] = false;
-		$rtn["errorCode"] ="尚未註冊";
-		$rtn["data"] = ""; 
-		echo json_encode($rtn);
-		exit; 
-	}else{ //有註冊
-		$row = mysql_fetch_array($query); 
-		$getpasstime = time(); 
-		$uid = $row['id']; 
-		$token = base64_encode($uid.$row['UserID'].$row['Password']);//組合驗證碼
-		$url = "reset.php?email=".$email."&token=".$token;//構造URL 
-		$time = date('Y-m-d H:i'); 
-		$result = sendmail($time, $email, $url); 
-		if($result==1){//郵件傳送成功  
-			$rtn = array();
-			$rtn["status"] = true;
-			$rtn["errorCode"] ="";
-			$rtn["data"] = "系統已向您的郵箱傳送了一封郵件<br/>請登入到您的郵箱及時重置您的密碼！"; 
-			echo json_encode($rtn);
-			/*$msg = '系統已向您的郵箱傳送了一封郵件<br/>請登入到您的郵箱及時重置您的密碼！'; 
-			更新資料傳送時間 
-			mysql_query("update `User` set `getpasstime`='$getpasstime' where id='$uid '"); */
-		}else{ $msg = $result; } 
-		//update password
-	$sql = "UPDATE `User` SET `Password` = '$token' WHERE `UserID`='$email'";
-	$result = $conn->query($sql);
-	if(!$result){
-	die($conn->error);
-	}
-//傳送郵件 
-	function sendmail($time,$email,$url){ 
-		require_once("smtp.class.php");
-		$smtpserver = ""; //SMTP伺服器，如smtp.163.com 
-		$smtpserverport = 25; //SMTP伺服器埠 
-		$smtpusermail = ""; //SMTP伺服器的使用者郵箱 
-		$smtpuser = ""; //SMTP伺服器的使用者帳號 
-		$smtppass = ""; //SMTP伺服器的使用者密碼 
-		$smtp = new Smtp($smtpserver, $smtpserverport, true, $smtpuser, $smtppass); //這裡面的一個true是表示使用身份驗證,否則不使用身份驗證. 
-		$emailtype = "HTML"; //信件型別，文字:text；網頁：HTML 
-		$smtpemailto = $email; 
-		$smtpemailfrom = $smtpusermail; 
-		$emailsubject = "Helen海大討論區系統 - 找回密碼"; 
-		$emailbody = "HI!!".$email."：<br/>您在".$time."提交了忘記密碼請求。請使用以下驗證碼為新的登入密碼，登入後請至個人頁面修改密碼。。<br/><a href='".$url."'target='_blank'>".$url."</a>"; 
-		$rs = $smtp->sendmail($smtpemailto, $smtpemailfrom, $emailsubject, $emailbody, $emailtype); 
-		return $rs; 
-}
-?>
+<?php include '../sendemail.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Contact Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	<!-- w3school的搜尋圖示 -->
+        
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif+TC&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.3/sweetalert2.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.3/sweetalert2.css" />
+        <!--
+        <link rel="icon" href="./static/icon128.ico" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif+TC&display=swap" rel="stylesheet">
+        <link rel= "stylesheet" href= "./static/myBookCaseMain.css">
+        -->
+        <link rel= "stylesheet" href= "../css/forgetPassword.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel= "stylesheet" href= "../css/topnav.css">
+  </head>
+  <body>
+  
+
+    <!--alert messages start-->
+    <?php echo $alert; ?>
+    <!--alert messages end-->
+
+    <!--contact section start-->
+    <!--<div class="contact-section">
+      <div class="contact-info">
+        <div><i class="fas fa-map-marker-alt"></i>Address, City, Country</div>
+        <div><i class="fas fa-envelope"></i>contact@email.com</div>
+        <div><i class="fas fa-phone"></i>+00 0000 000 000</div>
+        <div><i class="fas fa-clock"></i>Mon - Fri 8:00 AM to 5:00 PM</div>
+      </div>
+      <div class="contact-form">
+        <h2>Contact Us</h2>
+        <form class="contact" action="" method="post">
+          <input type="text" name="name" class="text-box" placeholder="Your Name" required>
+          <input type="email" name="email" class="text-box" placeholder="Your Email" required>
+          <input type="message" rows="5" placeholder="Your Message" required><input>
+          <input type="submit" name="submit" class="send-btn" value="Send">
+        </form>
+      </div>
+    </div>-->
+    <div class="topnav">
+        <div class="dropdown">
+        <button class="dropbtn" onclick="myFunction(), cross(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+        </button>
+        <div class="dropdown-content" id="myDropdown">	<!-- 各大版-->
+          <a href="#">Link 1</a>	
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+          <a href="#">+</a>
+        </div>
+        </div>
+        <div class="login">
+        <button>登入</button>
+        </div>
+        <div class="imgcontainer">
+          <i class="fa fa-user-circle" aria-hidden="true"></i>
+        </div>
+        <div class="search-container">
+          <input type="text" placeholder="Search.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </div>
+      </div>
+    <div class= "tabContent">
+        <h2>Helen－忘記密碼</h2>
+        <p>Welcome To Helen</p>
+        <div class="contentArea">
+            <div class= "content">
+        <table>
+        <form class="contact" action="" method="post">
+            <tr>
+                <td rowspan= "4" style="width: 200pt; height: 200pt">
+                    <a href={{ url_for('index') }}> 
+                        <img src= "../imgs/logo.png" class= "logo">
+                    </a>
+                </td>
+                <td>輸入帳號</td>
+
+                <td>
+                
+                    <span class="glyphicon glyphicon-envelope"></span>
+                    <!--<input type="email" name="email" class="text-box" placeholder="Your Email" required>-->
+                    <input type="text" name="email" id= "emailID" class="textInput"
+                        name= "email" placeholder="Enter email"  autocomplete="off" >
+                    <span id="emailMsg"></span>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <button type="submit" class="btn btn-info" name="submit" id="sub_btn">
+                        <span class="glyphicon glyphicon-send"></span> 驗證信箱</button>
+                        <span id="chkmsg"></span>
+                </td>
+            </tr>
+            </form>
+            <tr>
+            
+                <td>輸入新密碼</td>
+                <td>
+                    <span class="glyphicon glyphicon-lock"></span>
+                    <input type="password" id= "password" class="textInput"
+                    name= "password" placeholder="password" required>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <button type="button" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-log-in"></span> 登入
+                    </button>
+                </td>
+            </tr>
+        </table>
+    </div>
+        </div>
+    <!--contact section end-->
+
+    <script type="text/javascript">
+    if(window.history.replaceState){
+      window.history.replaceState(null, null, window.location.href);
+    }
+    </script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js">
+       </script>
+
+    <script src="../js/forgetPassword.js"></script>
+    
+    </body>
+</html>
+                           
---
+++ sendmail_login.php
@@ -0,0 +1,60 @@
+<?php
+// 前端 to 後端:
+// let cmd = {};
+// cmd["act"] = "changePassword";
+// cmd["account"] = "0123456";
+// cmd["password"] = "789789789";
+
+// 後端 to 前端
+// dataDB.status
+// dataDB.errorCode
+// 若 status = true:
+// dataDB.data="success to change the password"
+// 否則
+// dataDB.data = "fail to find the password"
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+//require_once 'index.php'; //連線資料庫
+require_once("test.php");
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$alert = '';
+
+if(isset($input['submit'])){
+//   $email = $_POST['email'];
+$email = $input['account'];
+ 
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'softwareengineeringhelen@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'soft123soft'; // Gmail address Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
+    $mail->setFrom('softwareengineeringhelen@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress($email."@mail.ntou.edu.tw"); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+	
+	//email內容
+    $mail->isHTML(true);
+    $mail->Subject = "=?UTF-8?B?".base64_encode('Helen海大討論區')."?=";
+    $mail->Body = "<div style='border-style:double;border-color:#0000C6;width:700px;'><h3>&nbsp;To whom it may concern,<br><br>&nbsp;&nbsp;&nbsp;&nbsp;you had registered in Helen Account.</h3></div>";
+	$mail->send();
+	//global $conn;
+	//例外
+    $alert = '<div class="alert-success">
+                 <span>Message Sent! Thank you for contacting us.</span>
+                </div>';
+  } catch (Exception $e){
+    $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
+}
+?>
+                           
