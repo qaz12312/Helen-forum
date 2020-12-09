dbfe6ee9ec0c059e5393818b83b7fa86f8c57ecc
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-09 19:01:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+    header("Content-Type:text/html; charset=utf-8");
+    date_default_timezone_set("Asia/Taipei");
+	error_reporting(1);
+	error_reporting(E_ALL);
+    // $input = json_decode(file_get_contents('php://input', 'r'), true); 
+    $input = $_POST;
+    require_once("connectDB.php");
+    $conn = connSql();
+    session_start();
+	switch($input["act"]){
+        case "logIn": //登入
+	        require_once("logIn.php");
+            doLogIn($input);
+            break;
+        case "browseAuthority": //獲得權限+所管理的版
+            require_once("browseAuthority.php");
+            doBrowseAuthority($input);
+            break;
+        case "creatAccount": //註冊
+            require_once("signUp.php");
+	        doCreatAccount($input);
+            break;
+        // case "forgetPassword": //忘記密碼
+        //     require_once("forgetPassword.php");
+        //     doForgetPassword($input);
+        //     break;
+        case "modifyPersonalInfo": //修改個資
+            require_once("personalInfo.php");
+            break;
+        
+        // case "home": // 系統首頁
+        //     home.php
+        //     break;
+        // case "publishArticle"://新增文章
+        //     publishArticle.php
+        // case "report":	//檢視檢舉
+        //     admin.php
+        //     break;
+    }
+?>
