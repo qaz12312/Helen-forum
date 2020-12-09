ca9bca408bdd0772b16dde3f92a17a5d46418874
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-09 20:14:48 +0800

    前端未接
    add deleteComment.php

--- PDO加密未完成/deleteComment.php
+++ PDO加密未完成/deleteComment.php
@@ -4,8 +4,8 @@
     let cmd = {};
     cmd["act"] = "deleteComment";
     cmd["token"] = "c93b3e8ab496d786030fbf8a17c3da51";
-    cmd["articleID"] ="ArticleID"
-    cmd["floors"] = "Floor"
+    cmd["articleID"] =ArticleID
+    cmd["floors"] = Floor
 
     後端 to 前端
     dataDB.status
