68a209d154b258d8c784ab09d1ee259f7cba2868
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-09 20:19:34 +0800

    前端未接
    updata summary

--- PDO加密未完成/deleteBoard.php
+++ PDO加密未完成/deleteBoard.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+    /* 
     前端 to 後端:
     let cmd = {};
     cmd["act"] = "deleteBoard";
@@ -15,56 +15,57 @@
         dataDB.errorCode = "You don't have permission to do this action." / "Your permission can't do this action." / "此版塊不存在" / "刪除失敗，資料庫異常"
         dataDB.data = ""
     */
-function doDeleteBoard($input)
-{
-    global $conn;
-    $token = $input['token'];
-    if (!isset($_SESSION[$token])) {
-        $rtn = array();
-        $rtn["status"] = false;
-        $rtn["errorCode"] = "You don't have permission to do this action.";
-        $rtn["data"] = "";
-    } else {
-        $userInfo = $_SESSION[$token];
-        if ($userInfo['permission'] < 3) {
+    function doDeleteBoard($input)
+    {
+        global $conn;
+        $token = $input['token'];
+        if (!isset($_SESSION[$token])) {
             $rtn = array();
             $rtn["status"] = false;
-            $rtn["errorCode"] = "Your permission can't do this action.";
+            $rtn["errorCode"] = "You don't have permission to do this action.";
             $rtn["data"] = "";
         } else {
-            $sqlcheck = "SELECT `BoardName` FROM `Board` NATURAL JOIN `Users`  WHERE `BoardName`='" . $input['boardName'] . "' AND `UserID`='" . $userInfo['account'] . "'";
-            $result = $conn->query($sqlcheck);
-            if (!$result) {
-                die($conn->error);
-            }
-            if ($result->num_rows <= 0) {
+            $userInfo = $_SESSION[$token];
+            if ($userInfo['permission'] < 3) {
                 $rtn = array();
                 $rtn["status"] = false;
-                $rtn["errorCode"] = "此版塊不存在";
+                $rtn["errorCode"] = "Your permission can't do this action.";
                 $rtn["data"] = "";
             } else {
-                $sql = "DELETE FROM `Board`  WHERE `BoardName`='" . $input['boardName'] . "' AND `UserID`='" . $userInfo['account'] . "'  ";
-                $result = $conn->query($sql);
+                $sqlcheck = "SELECT `BoardName` FROM `Board` NATURAL JOIN `Users`  WHERE `BoardName`='" . $input['boardName'] . "' AND `UserID`='" . $userInfo['account'] . "'";
+                $result = $conn->query($sqlcheck);
                 if (!$result) {
                     die($conn->error);
                 }
-                $sql = "SELECT `BoardName` FROM `Board` WHERE `BoardName` = '" . $input['boardName'] . "' ";
-                $result = $conn->query($sql);
-                if (!$result) {
-                    die($conn->error);
-                }
-                if ($result->num_rows > 0) {
+                if ($result->num_rows <= 0) {
                     $rtn = array();
                     $rtn["status"] = false;
-                    $rtn["errorCode"] = "刪除失敗，資料庫異常";
+                    $rtn["errorCode"] = "此版塊不存在";
                     $rtn["data"] = "";
                 } else {
-                    $rtn = array();
-                    $rtn["status"] = true;
-                    $rtn["errorCode"] = "成功刪除此版塊";
+                    $sql = "DELETE FROM `Board`  WHERE `BoardName`='" . $input['boardName'] . "' AND `UserID`='" . $userInfo['account'] . "'  ";
+                    $result = $conn->query($sql);
+                    if (!$result) {
+                        die($conn->error);
+                    }
+                    $sql = "SELECT `BoardName` FROM `Board` WHERE `BoardName` = '" . $input['boardName'] . "' ";
+                    $result = $conn->query($sql);
+                    if (!$result) {
+                        die($conn->error);
+                    }
+                    if ($result->num_rows > 0) {
+                        $rtn = array();
+                        $rtn["status"] = false;
+                        $rtn["errorCode"] = "刪除失敗，資料庫異常";
+                        $rtn["data"] = "";
+                    } else {
+                        $rtn = array();
+                        $rtn["status"] = true;
+                        $rtn["errorCode"] = "成功刪除此版塊";
+                    }
                 }
             }
         }
+        echo json_encode($rtn);
     }
-    echo json_encode($rtn);
-}
+?>
