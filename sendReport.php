b64f80cee604ca41ac027b5b6e5d029d7bb7af58
00757033 <lspss95270@gmail.com>
2020-12-10 01:21:56 +0800

    ok: test all

--- sendReport.php
+++ sendReport.php
@@ -1,10 +1,10 @@
 <?php
-    require_once 'connectDB.php'; //連線資料庫 
+    //require_once 'connectDB.php'; //連線資料庫 
 /* 前端 to 後端:
             let cmd = {};
             cmd["act"] = "sendReport";
             cmd["reason"] = "Reason"
-			cmd["timer"] = "Times"
+			cmd["account"] = "userid"
         */
 		
         /* 後端 to 前端
@@ -17,30 +17,32 @@
             否則
                 dataDB.data = ""
          */
-    global $input,$conn;
-    $new="INSERT INTO  `Report`(`ArticleID`,`Reason`,`Times`) 
-    VALUES('".$input['articleID']."','".$input['reason']."'','".$input['timer']."')";
-    $resultNew=$conn->query($new);
-    if(!$resultNEW){
-        die($conn->error);
+    function doSendReport($input){
+        global $conn;
+        $new="INSERT INTO  `Report`(`UserID`,`ArticleID`,`Reason`) 
+        VALUES('".$input['account']."','".$input['articleID']."','".$input['reason']."')";
+        $resultNew=$conn->query($new);
+        if(!$resultNew){
+            die($conn->error);
+        }
+        $sql="SELECT `ArticleID`,`Reason`,`Times` FROM `Report` WHERE `ArticleID`='".$input['articleID']."' AND `reason`='".$input['reason']."' ";
+        $result=$conn->query($sql);
+        if(!$result){
+            die($conn->error);
+        }
+        if($result->num_rows <= 0){
+            $rtn = array();
+            $rtn["status"] = false;
+            $rtn["errorCode"] = "傳送檢舉失敗";
+            $rtn["data"] = "";
+        }
+        else{
+            $row=$result->fetch_row();
+            $rtn = array();
+            $rtn["status"] = true;
+            $rtn["errorCode"] = "";
+            $rtn["data"] = $row;
+        }
+        echo json_encode($rtn);
     }
-    $sql="SELECT `ArticleID`,`Reason`,`Times` FROM `Report` WHERE `ArticleID`=$input['articleID'] AND `reason`=$input['Reason'] ";
-    $result=$conn->query($sql);
-    if(!$result){
-        die($conn->error);
-    }
-    if($result->num_rows <= 0){
-        $rtn = array();
-        $rtn["status"] = false;
-        $rtn["errorCode"] = "傳送檢舉失敗";
-        $rtn["data"] = "";
-    }
-    else{
-        $row=$result->fetch_row();
-        $rtn = array();
-        $rtn["status"] = true;
-        $rtn["errorCode"] = "";
-        $rtn["data"] = $row;
-    }
-    echo json_encode($rtn);
 ?>
