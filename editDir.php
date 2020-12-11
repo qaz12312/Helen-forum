aa6102b2eec55783afbb874679bfd43f85c4f6d8
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-11 22:11:13 +0800

    Update editDir.php

--- editDir.php
+++ editDir.php
@@ -1,23 +1,21 @@
 <?php
-    /* 
-    前端 to 後端:
-            let cmd = {};
-            cmd["act"] = "editDir";
-            cmd["account"] = "UserID"
-            cmd["old"] ="我喜歡的"
-            cmd["new"] ="我不喜歡的"
+	/* 
+	前端 to 後端:
+	let cmd = {};
+	cmd["act"] = "editDir";
+	cmd["account"] = "UserID";
+	cmd["old"] ="我喜歡的"; (oldDirName)
+	cmd["new"] ="我不喜歡的"; (newDirName)
 
-        */
-		
-         /* 後端 to 前端
-            
-            若 status = true:
-                dataDB.data=更新後的資料夾名稱
-				dataDB.status = true:
-            否則
-                dataDB.data=""
-                dataDB.errorCode = 無權限修改 / 修改失敗
-         */
+	後端 to 前端
+	dataDB.status
+	若 status = true:
+		dataDB.errorCode = ""
+		dataDB.data=更新後的資料夾名稱
+	否則
+		dataDB.data=""
+		dataDB.errorCode = 無權限修改 / 修改失敗
+	*/
     function doEditDir($input){
         global $conn;
         $sqlcheck="SELECT `DirName`,`UserID` FROM `KeepDir` WHERE `DirName`='".$input['old']."' AND `UserID`='".$input['account']."' ";  
