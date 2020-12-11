ccebed93c3f683ab8295bb818ce82723322d5dbb
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-11 23:32:57 +0800

    Update sendNotification.php

--- sendNotification.php
+++ sendNotification.php
@@ -1,31 +1,30 @@
 <?php
-    //require_once 'connectDB.php'; //連線資料庫 
-/* 前端 to 後端:
-            let cmd = {};
-            cmd["act"] = "sendNotification";
-            cmd["userID"] = "傳給誰"
-			cmd["timer"] = "Times"
-            cmd["content"] = "Content"
-        */
-		
-        /* 後端 to 前端
-            dataDB.status
-            dataDB.errorCode
-            若 status = true:
-				dataDB.data[0]	// UserID
-				dataDB.data[1]	// Times
-				dataDB.data[2]	// Content
-            否則
-                dataDB.data = ""
-         */
+/* 
+前端 to 後端:
+let cmd = {};
+cmd["act"] = "sendNotification";
+cmd["recipient"] = "收通知的account";
+cmd["timer"] = "Times"(是否不需要呢?)
+cmd["content"] = "Content"
+
+後端 to 前端:
+dataDB.status
+dataDB.errorCode
+若 status = true:
+dataDB.data[0]	// UserID
+dataDB.data[1]	// Times
+dataDB.data[2]	// Content
+否則
+dataDB.data = ""
+*/
     function doSendNotification($input){
         global $conn;
-        $new="INSERT INTO  `Notice`(`UserID`,`Content`)VALUES('".$input['account']."','".$input['content']."')";
+        $new="INSERT INTO  `Notice`(`UserID`,`Content`)VALUES('".$input['recipient']."','".$input['content']."')";
         $resultNew=$conn->query($new);
         if(!$resultNew){
             die($conn->error);
         }
-        $sql="SELECT `UserID`,`Times`,`Content` FROM `Notice` WHERE `UserID`='".$input['account']."' AND`Content`='".$input['content']."' ";
+        $sql="SELECT `UserID`,`Times`,`Content` FROM `Notice` WHERE `UserID`='".$input['recipient']."' AND`Content`='".$input['content']."' ";
         $result=$conn->query($sql);
         if(!$result){
             die($conn->error);
