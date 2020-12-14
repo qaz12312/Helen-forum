41d79c7825f0d96f5def068ff097b392a5934162
PollyYang <watermoo22@gmail.com>
2020-12-14 18:34:28 +0800

    delete

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    require_once("home.html"); 
     header("Access-Control-Allow-Origin: *");
     header("Content-Type:text/html; charset=utf-8");
     date_default_timezone_set("Asia/Taipei");
