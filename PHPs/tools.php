b3f194f637bff9831473980ed67017c0470d383f
PollyYang <watermoo22@gmail.com>
2020-12-21 22:45:13 +0800

    add

--- PHPs/tools.php
+++ PHPs/tools.php
@@ -45,22 +45,32 @@ function query($conn,$sql,$input,$option){
 <?php
     /*
     account // 資料夾名稱
-    time // 檔名(2020/12/21 22-15)
+    time // 檔名(2020-12-21 22-15)
     info // 要 寫進檔案的內容
     */
     // log
-    function writeLog($info){
+    function writeRecord($info){
         $TxtFileName = "./Data/Record/".$info["account"]."/".$info["time"].".txt";
         //以讀寫方式打寫指定檔案，如果檔案不存則建立
-        if( ($TxtRes=fopen ($TxtFileName,"a")) === FALSE){
+        if( ($file=fopen ($TxtFileName,"a")) === FALSE){
             return "建立可寫檔案：".$TxtFileName."失敗";
             exit();
         }
-        if(!fwrite ($TxtRes,$input["info"]."\n")){ //將資訊寫入檔案
-            fclose($TxtRes);
+        if(!fwrite ($file,$info["info"]."\n")){ //將資訊寫入檔案
+            fclose($file);
             return "嘗試向檔案".$TxtFileName."寫入".$StrConents."失敗！";
             exit();
         }
-        fclose ($TxtRes); //關閉指標
+        fclose ($file); //關閉指標
+    }
+    /*
+    account // 資料夾名稱
+     */
+    // dir
+    function newUserFolder($account){
+        $root = "./Data/Record/".$account;
+        if (!is_dir($root)){
+            mkdir($root,0777,true);
+        }
     }
 ?>
