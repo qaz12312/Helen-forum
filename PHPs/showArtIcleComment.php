3ca209612f5ee00a1f6937461bde4a2418bf5a1a
00757033 <58459235+00757033@users.noreply.github.com>
2020-12-21 19:29:31 +0800

    update php

--- PHPs/showArtIcleComment.php
+++ PHPs/showArtIcleComment.php
@@ -17,8 +17,11 @@
         dataDB.data.comment[i].floor //第i筆留言的樓層
         dataDB.data.comment[i].nickname
         dataDB.data.comment[i].color
+        dataDB.data.comment[i].time
         ) 
         dataDB.data.title //第i筆文章的標題
+        dataDB.data.content 
+        dataDB.data.time
         dataDB.data.like //第i筆文章的總愛心數
         dataDB.data.keep//第i筆文章的總收藏數
         dataDB.data.nickname
@@ -29,7 +32,7 @@
     */
     function doShowArticleComment($input){
         global $conn;
-        $sql="SELECT `Title`,`Content`,`BoardName`,`ArticleID` ,`cntHeart` ,`cntKeep` FROM HomeHeart NATURAL JOIN HomeKeep WHERE `ArticleID`=?";
+        $sql="SELECT `Title`,`Content`,`BoardName`,`ArticleID` ,`cntHeart` ,`cntKeep`,`Times` FROM HomeHeart NATURAL JOIN HomeKeep WHERE `ArticleID`=?";
         $arr = array($input['articleID']);
         $result = query($conn,$sql,$arr,"SELECT");
         $resultCount = count($result);
@@ -51,11 +54,11 @@
                 $arr = array($articleID, $input['account']);
                 $keep = query($conn,$sql,$arr,"SELECT");
                 $keepCount = count($keep);
-                $arr = array("title"=>$result[0][0],"content"=>$result[0][1],"blockName"=>$result[0][2],"articleID"=>$result[0][3],"like"=>$result[0][4],"keep"=>$result[0][5], "hasHeart" => ( $heartCount>0 ? 1 : 0), "hasKeep" => ($keepCount>0 ? 1 : 0 ));
+                $arr = array("title"=>$result[0][0],"content"=>$result[0][1],"blockName"=>$result[0][2],"articleID"=>$result[0][3],"like"=>$result[0][4],"keep"=>$result[0][5],"time"=>$result[0][6],"hasHeart" => ( $heartCount>0 ? 1 : 0), "hasKeep" => ($keepCount>0 ? 1 : 0 ));
             }else
-                $arr = array("title"=>$result[0][0],"content"=>$result[0][1],"blockName"=>$result[0][2],"articleID"=>$result[0][3],"like"=>$result[0][4],"keep"=>$result[0][5], "hasHeart" => "", "hasKeep" =>"");
+                $arr = array("title"=>$result[0][0],"content"=>$result[0][1],"blockName"=>$result[0][2],"articleID"=>$result[0][3],"like"=>$result[0][4],"keep"=>$result[0][5],"time"=>$result[0][6], "hasHeart" => "", "hasKeep" =>"");
 
-            $sql ="SELECT `Nickname`, `Color`,`Content`,`Floor` FROM Comments JOIN Users ON Users.UserID=Comments.AuthorID WHERE `ArticleID`=? order by Floor ASC " ;
+            $sql ="SELECT `Nickname`, `Color`,`Content`,`Floor`,`Times` FROM Comments JOIN Users ON Users.UserID=Comments.AuthorID WHERE `ArticleID`=? order by Floor ASC " ;
             $arr3 = array($articleID);
             $comment = query($conn,$sql,$arr3,"SELECT");
             $commentCount = count($comment);
@@ -66,7 +69,7 @@
             else{
                 for($i=0;$i<$commentCount;$i++){
                     $row = $comment[$i];
-                    $arr2[$i]=array("nickname"=>$row[0],"color"=>$row[1],"content"=>$row[2],"Floor"=>$row[3] );
+                    $arr2[$i]=array("nickname"=>$row[0],"color"=>$row[1],"content"=>$row[2],"floor"=>$row[3],"time"=>$row[4]);
                 }
             }
             $arr['comment']=$arr2;
