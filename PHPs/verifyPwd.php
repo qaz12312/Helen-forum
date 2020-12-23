c56edf07ff3c27231da6bce814e81b3f0fff2d26
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-23 23:50:06 +0800

    Update verifyPwd.php

--- PHPs/verifyPwd.php
+++ PHPs/verifyPwd.php
@@ -18,12 +18,12 @@
         global $conn;
         if(isset($_SESSION[$input['token']])){
             $arr = $_SESSION[$input['token']];
-            $account = $arr['account'];
+            $token = base64_encode($arr['account']);
             $last = $arr['time'];   //過期時間
             $now = date ("Y-m-d H:i:s" , mktime(date('H')+7, date('i'), date('s'), date('m'), date('d'), date('Y')));   //現在時間
             unset($_SESSION[$input['token']]);
             if((strtotime($last) - strtotime($now)) < 900){    //沒過期
-                $rtn = successCode("success to verify",$account);
+                $rtn = successCode("success to verify",$token);
             }else{
                 errorCode("fail to verify");
             }
