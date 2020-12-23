c625c4c4ba7ea4701d274b5be66befc5e7082d9e
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-23 23:52:30 +0800

    Update verifyPwd.php

--- PHPs/verifyPwd.php
+++ PHPs/verifyPwd.php
@@ -9,7 +9,7 @@
     dataDB.status
     若 status = true:
         dataDB.info = "success to verify"
-        dataDB.data = token
+        dataDB.data = account
     否則
         dataDB.errorCode = "fail to verify"
         dataDB.data = ""
@@ -18,12 +18,12 @@
         global $conn;
         if(isset($_SESSION[$input['token']])){
             $arr = $_SESSION[$input['token']];
-            $token = base64_encode($arr['account']);
+            $account = $arr['account'];
             $last = $arr['time'];   //過期時間
             $now = date ("Y-m-d H:i:s" , mktime(date('H')+7, date('i'), date('s'), date('m'), date('d'), date('Y')));   //現在時間
             unset($_SESSION[$input['token']]);
             if((strtotime($last) - strtotime($now)) < 900){    //沒過期
-                $rtn = successCode("success to verify",$token);
+                $rtn = successCode("success to verify",$account);
             }else{
                 errorCode("fail to verify");
             }
