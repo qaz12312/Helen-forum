e7200cab7f1c0b1c0c2467294bea0aff9c283bc3
acs107102 <43445853+acs107102@users.noreply.github.com>
2020-12-23 22:07:13 +0800

    Update

--- PHPs/searchInMenu.php
+++ PHPs/searchInMenu.php
@@ -4,8 +4,8 @@
     let cmd = {};
     cmd["act"] = "searchMenu";
     cmd["account"]="00757033";
-    cmd["searchWord"] = "美食";
-    cmd["sort"] = "time/hot";
+    cmd["searchWord"] = ["美食"];
+    cmd["sort"] = "time/hot/collect/comment";
 
     後端 to 前端:
     dataDB = JSON.parse(data);
@@ -29,26 +29,31 @@
     function doSearchMenu($input){
         global $conn;
         //搜尋標題+內容
-        if ($input['sort'] == "time" || $input['sort'] == "hot" || $input['sort'] == "collect") {
-            $sql = "SELECT `Title`,`BoardName`,`ArticleID` ,`cntHeart` ,`cntKeep` FROM HomeHeart NATURAL JOIN HomeKeep WHERE";
+        if ($input['sort'] == "time" || $input['sort'] == "hot" || $input['sort'] == "collect" || $input['sort'] == "comment") {
+            $sql = "SELECT `Title`,`BoardName`,`ArticleID` ,`cntHeart` ,`cntKeep` FROM HomeHeart NATURAL JOIN HomeKeep";
+			if ($input['sort'] == "comment") {
+				$sql = $sql. " LEFT JOIN `HomeComment` USING (ArticleID)";
+			}
+			$sql = $sql. " WHERE ";
             $arrsize = count($input["searchWord"]);
             $sql = $sql .str_repeat("`Content` LIKE ? OR ",  $arrsize-1);
             $sql = $sql . "`Content` LIKE ? OR  ";
             $sql = $sql .str_repeat("`Title` LIKE  ? OR ",  $arrsize-1);
-            $sql = $sql . "`Title` LIKE  ?  AND `BoardName` =?";
+            $sql = $sql . "`Title` LIKE  ?";
             if ($input['sort'] == "time") 
                 $sql = $sql . " ORDER BY `Times` DESC;";
             else if ($input['sort'] == "hot")
                 $sql = $sql . " ORDER BY `cntHeart` DESC;";
-            else
+            else if ($input['sort'] == "collect")
                 $sql = $sql . " ORDER BY `cntKeep` DESC;";
+			else 
+				$sql = $sql . " ORDER BY `cntComment` DESC;";
             $i = 0;
             for($j=0;$j<2;$j++){
                 foreach($input["searchWord"] as $value){
                 $search[$i++] = "%".$value."%";
                 }
             }
-            $search[$i] = "%".$input['searchBoard']."%";
             $arr = $search;
             $result = query($conn, $sql, $arr, "SELECT");
             $resultCount = count($result);
