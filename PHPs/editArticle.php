02e05fc5e75b22fb924f2e76f708a23ad138e0ed
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2020-12-28 21:53:02 +0800

    update hashTag

--- PHPs/editArticle.php
+++ PHPs/editArticle.php
@@ -4,23 +4,22 @@
 	let cmd = {};
 	cmd["act"] = "editArticle";
 	cmd["articleID"] = ArticleID;
-	cmd["account"] = "AuthorID"; //cmd["token"]
+	cmd["account"] = "00757003"; //cmd["token"]
 	cmd["newBlockName"] ="美食"; // 使用者想改版
 	cmd["title"] = "Title";
 	cmd["content"] = "Content";
 	cmd["picture"] = "Image";
 	cmd["hashTag"] ="HashTag";
 
-	後端 to 前端
+	後端 to 前端:
+	dataDB = JSON.parse(data);
 	dataDB.status
 	若 status = true:
-		dataDB.status = true
 		dataDB.info = "Successfully edited this article."
 		dataDB.data = 
-	否則 status = false:
-		dataDB.status = false
-		dataDB.errorCode = "Update without permission."/"Failed to Update Article,Database exception.";
-		dataDB.data = "Update without permission. / Failed to Update Article, Database exception."
+	否則
+		dataDB.errorCode = "Update without permission."/"Failed to Update Article,Database exception."
+		dataDB.data = 
 	*/
     function doEditArticle($input){ 
 		global $conn;    
@@ -29,30 +28,35 @@
 		// 	errorCode("token doesn't exist.");
         // }else{
 		// 	$userInfo = $_SESSION[$token];
-		$sql="SELECT `ArticleID` FROM `Article` JOIN `Users` ON Users.UserID=Article.AuthorID WHERE `ArticleID`=? AND `AuthorID`=?";  
-		$arr = array($input['articleID'], $input['account']);
-		$result = query($conn,$sql,$arr,"SELECT");
-		$resultCount = count($result);
-		if($resultCount <= 0){
-			errorCode("Update without permission.");
-		}
-		else{
-			$sql="UPDATE `Article` SET `Title`=?,`Content`=?,`Image`=?,`HashTag`=?,`BlockName`=? WHERE `ArticleID` = ? AND `AuthorID`=?";
-			$arr = array($input['title'], $input['content'], $input['picture'], $input['hashTag'], $input['newBlockName'], $input['articleID'], $input['account']);
-			query($conn,$sql,$arr,"UPDATE");
-
-			$sql="SELECT `ArticleID`,`Title`,`Content`,`Image`,`HashTag`,`Times`,`Color`,`BlockName` FROM `Article` JOIN`Users` ON Users.UserID=Article.AuthorID WHERE `Title`=? AND `Content`=? AND `Image`=? AND `HashTag`=?";
-			$arr = array($input['title'], $input['content'], $input['picture'], $input['hashTag']);
+			// $user = $userInfo['account'];
+			$sql="SELECT `ArticleID` FROM `Article` JOIN `Users` ON Users.UserID=Article.AuthorID WHERE `ArticleID`=? AND `AuthorID`=?";  
+			// $arr = array($input['articleID'], $user);
+			$arr = array($input['articleID'], $input['account']);
 			$result = query($conn,$sql,$arr,"SELECT");
-
 			$resultCount = count($result);
 			if($resultCount <= 0){
-				errorCode("Failed to Update Article, Database exception.");
+				errorCode("Update without permission.");
 			}
 			else{
-				$rtn = successCode("Successfully edited this article.", $result);
+				$hashTag = json_encode($input['hashTag']);
+				// $hashTag = $input['hashTag'];
+				$sql="UPDATE `Article` SET `Title`=?,`Content`=?,`Image`=?,`HashTag`=?,`BlockName`=? WHERE `ArticleID` = ? AND `AuthorID`=?";
+				// $arr = array($input['title'], $input['content'], $input['picture'], $hashTag, $input['newBlockName'], $input['articleID'], $user);
+				$arr = array($input['title'], $input['content'], $input['picture'], $hashTag, $input['newBlockName'], $input['articleID'], $input['account']);
+				query($conn,$sql,$arr,"UPDATE");
+
+				$sql="SELECT `ArticleID`,`Title`,`Content`,`Image`,`HashTag`,`Times`,`Color`,`BlockName` FROM `Article` JOIN`Users` ON Users.UserID=Article.AuthorID WHERE `Title`=? AND `Content`=? AND `Image`=? AND `HashTag`=?";
+				$arr = array($input['title'], $input['content'], $input['picture'], $hashTag);
+				$result = query($conn,$sql,$arr,"SELECT");
+				$resultCount = count($result);
+				if($resultCount <= 0){
+					errorCode("Failed to Update Article, Database exception.");
+				}
+				else{
+					$rtn = successCode("Successfully edited this article.", $result);
+				}
 			}
-		}
-		echo json_encode($rtn);
+			echo json_encode($rtn);
+		// }
 	}
 ?>
--- PHPs/newArticle.php
+++ PHPs/newArticle.php
@@ -3,7 +3,7 @@
 	前端 to 後端:
 	let cmd = {};
 	cmd["act"] = "newArticle";
-	cmd["account"] = "AuthorID"; //cmd["token"]
+	cmd["account"] = "00757003"; //cmd["token"]
 	cmd["blockName"] ="美食";
 	cmd["title"] = "Title";
 	cmd["content"] = "Content"
@@ -11,9 +11,9 @@
 	cmd["hashTag"] ="HashTag"
 	
 	後端 to 前端:
+	dataDB = JSON.parse(data);
 	dataDB.status
     若 status = true:
-        dataDB.status = true
 		dataDB.info = ""
 		dataDB.data[0]	// ArticleID
 		dataDB.data[1]	// Title
@@ -22,8 +22,7 @@
 		dataDB.data[4]	// HashTag
 		dataDB.data[5]	// Time
 		dataDB.data[6]	// Color
-    否則 status = false:
-        dataDB.status = false
+    否則
 		dataDB.errorCode = "Failed to upload article,Database exception."
 		dataDB.data = ""
 	*/
@@ -34,30 +33,28 @@
 		// 	errorCode("token doesn't exist.");
         // }else{
 		// 	$userInfo = $_SESSION[$token];
-        $sql="INSERT INTO  `Article`(`AuthorID`,`Title`,`Content`,`Image`,`HashTag`,`BlockName`) VALUES(?,?,?,?,?,?)";
-        
-		//$hashTag = json_encode($input['hashTag']);
-		$hashTag = $input['hashTag'];
-		$arr = array($input['account'], $input['title'], $input['content'], $input['picture'], $hashTag, $input['blockName']);
-		$result = query($conn,$sql,$arr,"INSERT");
-		$rtn = successCode("Successfully new the Article.",array());
-		
-		/*
-
-		流水號問題!!!!!!!!!!!!!!!!!
-
-        $articleID=mysqli_insert_id($conn);//取得流水號
-        $sql="SELECT `ArticleID`,`Title`,`Content`,`Image`,`HashTag`,`Times`,`Color` FROM `Article`  JOIN `Users` ON Users.UserID=Article.AuthorID WHERE `ArticleID`=?";
-        $arr = array($articleID);
-        $result = query($conn,$sql,$arr,"SELECT");
-        $resultCount = count($result);
-        if($resultCount <= 0){
-            errorCode("Failed to upload article,Database exception.");
-        }
-        else{
-            $rtn = successCode("Successfully new the Article.",$result[0]);
-		}
-		*/
-        echo json_encode($rtn);
+			$sql="INSERT INTO  `Article`(`AuthorID`,`Title`,`Content`,`Image`,`HashTag`,`BlockName`) VALUES(?,?,?,?,?,?)";
+			$hashTag = json_encode($input['hashTag']);
+			// $hashTag = $input['hashTag'];
+			//$arr = array($userInfo['account'], $input['title'], $input['content'], $input['picture'], $hashTag, $input['blockName']);
+			$arr = array($input['account'], $input['title'], $input['content'], $input['picture'], $hashTag, $input['blockName']);
+			$result = query($conn,$sql,$arr,"INSERT");
+			$rtn = successCode("Successfully new the Article.",array());
+			/*
+			流水號問題!!!!!!!!!!!!!!!!!
+			$articleID=mysqli_insert_id($conn);//取得流水號
+			$sql="SELECT `ArticleID`,`Title`,`Content`,`Image`,`HashTag`,`Times`,`Color` FROM `Article`  JOIN `Users` ON Users.UserID=Article.AuthorID WHERE `ArticleID`=?";
+			$arr = array($articleID);
+			$result = query($conn,$sql,$arr,"SELECT");
+			$resultCount = count($result);
+			if($resultCount <= 0){
+				errorCode("Failed to upload article,Database exception.");
+			}
+			else{
+				$rtn = successCode("Successfully new the Article.",$result[0]);
+			}
+			*/
+			echo json_encode($rtn);
+		// }
     }
 ?>
