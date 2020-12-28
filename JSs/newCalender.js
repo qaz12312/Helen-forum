a09c6de17c708fbb8c05ac805037eba24061ec1e
00757033 <lspss95270@gmail.com>
2020-12-29 02:18:23 +0800

    json成功

--- JSs/newCalender.js
+++ JSs/newCalender.js
@@ -1,38 +1,34 @@
-var fileStr; // add picture
-var borads= [];
-var hashtags= [];
 
 document.querySelector("html").classList.add('js');
 
-var fileInput  = document.querySelector( ".input-file" ),  
-    button     = document.querySelector( ".input-file-trigger" ),
-    the_return = document.querySelector( ".file-return");
     
-button.addEventListener( "keydown", function( event ) {  
-    if ( event.keyCode == 13 || event.keyCode == 32 ) {  
-        fileInput.focus();  
-    }  
-});
-button.addEventListener( "click", function( event ) {
-    fileInput.focus();
-    return false;
-});  
-fileInput.addEventListener( "change", function( event ) { 
-    var fileStr= this.value;
-    fileStr= fileStr.substring(12, fileStr.length);
-    the_return.innerHTML = fileStr;
-});
-
 $(document).ready(async function(){
     barInitial();
     await new Promise((resolve, reject) => initial(resolve, reject));
 })
 
-$("#publishBtn").on("click", function(){
+$("#allday").click(function () {
+    if ($(this).prop("checked")) {
+        //$("#activityStartTime").attr('disabled','disabled');
+        document.getElementById("activityStartTime").disabled = true;
+        document.getElementById("activityEndTime").disabled = true;
+        document.getElementById("activityEndDate").disabled = true;
+        $(this).val("true");
+    } else {
+        document.getElementById("activityStartTime").disabled = false;
+        document.getElementById("activityEndTime").disabled = false;
+        document.getElementById("activityEndDate").disabled = false;
+        $(this).val("false");
+    }
+});
+$("#allday").on("click", function(){
+    
+})  
+$("#newActivity").on("click", function(){
     console.log("I want publish.");
-    var titleStr= $("#articleTitle").val().trim();
-    var contentStr= $("#articleContent").val().trim();
-
+    var titleStr= $("#activityTitle").val().trim();
+    var contentStr= $("#activityContent").val().trim();
+    
     if(titleStr.length< 1){
         console.log("(Title) Too short.")
         swal({
@@ -67,22 +63,66 @@ $("#publishBtn").on("click", function(){
         }).then(( result ) => {}, ( dismiss ) => {});
         return;
     }
-    
+    if($("#allday").val()=="false" && (!$("#activityStartTime").val() ||!$("#activityEndTime").val())){
+        swal({
+            title: "請輸入時間或勾選全天！",
+            type: "warning",
+            // text: dataDB.errorCode
+        }).then(( result ) => {}, ( dismiss ) => {});
+        return;
+    }
+    if(!$("#activityStartDate").val()){
+        swal({
+            title: "請輸入開始日期！",
+            type: "warning",
+            // text: dataDB.errorCode
+        }).then(( result ) => {}, ( dismiss ) => {});
+        return;
+    }
+    if($("#allday").val()=="false" && !$("#activityEndDate").val()){
+        swal({
+            title: "請輸入結束日期！",
+            type: "warning",
+            // text: dataDB.errorCode
+        }).then(( result ) => {}, ( dismiss ) => {});
+        return;
+    }else if($("#allday").val()=="false"){
+        $("#activityEndDate").val($("#activityStartDate").val());
+    }
+        var startDate;
+        var endDate;
+    if($("#activityStartTime").val())
+        startDate= $("#activityStartDate").val()+"T"+$("#activityStartTime").val();
+    else
+        startDate= $("#activityStartDate").val()+"T"+"00:00:00";
+
+    if($("#activityEndTime").val())
+        endDate= $("#activityEndDate").val()+"T"+$("#activityEndTime").val();
+    else if($("#allday").val()=="true")
+        endDate= $("#activityStartDate").val()+"T"+"23:00:00";
+
+        var myStartDate = new Date(startDate);
+        var myEndDate = new Date(endDate);
+        var now=new Date();
+        alert(myStartDate)
+        if(!(myStartDate <= myEndDate)||myEndDate<=now||myStartDate<=now){
+            swal({
+                title: "日期或時間輸入錯誤請重新輸入！",
+                type: "warning",
+                // text: dataDB.errorCode
+            }).then(( result ) => {}, ( dismiss ) => {});
+            return;
+        }
+
     let cmd= {};
-    if(sessionStorage.getItem("Helen-act")== "editArticle"){ // 編輯文章
-        cmd["articleID"]= sessionStorage.getItem("Helen-articleID");
-        cmd["act"]= "editArticle";
-    }
-    else{ //發布文章
-        cmd["act"]= "newArticle";
-    }
-    cmd["account"]= sessionStorage.getItem("Helen-account")
-    cmd["title"]= titleStr;
-    let chooseStr= $("#chooseBoard :selected").val();
-    cmd["blockName"]= chooseStr.substring(0, chooseStr.length- 1);//text()
-    cmd["content"]= contentStr;
-    cmd["hashTag"]= hashtags;
-    cmd["picture"]= "Image"; // no picture
+    cmd["act"]= "newActivityInCanlendar";
+    cmd["account"]= sessionStorage.getItem("Helen-account");
+    cmd["title"] =titleStr;
+    
+
+    cmd["startTime"] =startDate;
+    cmd["endTime"] = endDate;
+    cmd["text"] =  $("#activityContent").val();
     console.log(cmd);
     
     $.post("../index.php", cmd, function(dataDB){
@@ -104,55 +144,17 @@ $("#publishBtn").on("click", function(){
     })
 })
 
-$("#cancelPublish").on("click", function(){
+$("#cancelActivity").on("click", function(){//取消
     console.log("Back To HOME Page.")
     location.href =  "../HTMLs/home.html";
 })
 
-$("#inputHashtag").keypress(function (event){
-    var hashtagStr= $("#inputHashtag").val().trim();
-    if(event.keyCode== 13){
-        if(hashtags.length>= 5){
-            console.log(hashtags+ " >= 5");
-        }else if(hashtagStr.length<= 0){
-            console.log("no Input");
-        }else if(hashtagStr== "#"){
-            console.log("no Input");
-        }else{
-            if(hashtagStr[0] == "#"){
-                hashtagStr= hashtagStr.substring(1, hashtagStr.length).trim();
-            }
-            console.log(hashtagStr)
-            hashtags.push(hashtagStr);
-            $("#inputHashtag").val("#");
-            printHashtag();
-        }
-        console.log(hashtags);
-    }
-})
-
-$("#hashtags").on( "click", "#clearHashtags", function(){
-    hashtags= [];
-    printHashtag();
-});
-
-function printHashtag(){
-    $("#hashtags").empty();
-    var temp= "";
-    for(let h in hashtags){
-        temp+= '<span class="badge badge-pill">#'+ hashtags[h]+ "</span> ";
-    }
-    if(temp!= ""){
-        temp= '<span id= "clearHashtags" class="glyphicon glyphicon-remove"></span> '+ temp;
-    }
-    $("#hashtags").append(temp);
-}
 
 async function initial(res, rej){
     let r = await new Promise((resolve, reject) => checkPermission(resolve, reject)
          // 未登入
     );
-
+    $("#activityStartDate").val(sessionStorage.getItem("Helen-startTime"));
     if(!r) return;
 
     //selector 載入所有看版(從 session)
@@ -162,41 +164,6 @@ async function initial(res, rej){
     $("#chooseBoard").empty();
     for(var i= 0; i< boards.length; i++){
         $("#chooseBoard").append(new Option(boards[i]+ "版"), i, false);
-    }
-
-    //編輯文章
-    //前 -> 後：{動作:編輯文章, 文章ID: 201}
-    // !要取得文章ID!
-    //後 -> 前：{"文章所屬看版", "文章標題", "文章內容", [文章的hastags], ?圖片?}
-    if(sessionStorage.getItem("Helen-act")== "editArticle"){
-        let cmd= {};
-        cmd["act"]= "showArticleComment";
-        cmd["account"]= sessionStorage.getItem("Helen-account");
-        cmd["articleID"]= sessionStorage.getItem("Helen-articleID");
-
-        $.post("../index.php", cmd, function(dataDB){
-            dataDB= JSON.parse(dataDB);
-
-            if(dataDB.status == false){
-                swal({
-                    title: "載入頁面失敗",
-                    type: "error",
-                    text: dataDB.errorCode
-                }).then(( result ) => {}, ( dismiss ) => {});
-            }
-            else{
-                article= dataDB.data
-                $(".tabContent").find("h2").text("Helen－編輯文章");
-                $(".tabContent").find("p").text("Edit your post.");
-                //從後端拿資料
-                $("#chooseBoard").find("option[text= '" + article.blockName+ "版']").attr("selected", true);
-                //依 text 為"看版名(美食版)"的項選中
-                $("#articleTitle").val(article.title);
-                $("#articleContent").val(article.content);
-                hashtags= article.hashtag;
-                printHashtag();
-            }
-        })
     }
     res(0);
 }
