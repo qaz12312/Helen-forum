7df3cff35aaf4569c4b6345ac8da02a2969b2661
00757033 <58459235+00757033@users.noreply.github.com>
2020-12-30 23:17:34 +0800

    update

--- PHPs/showBoardList.php
+++ PHPs/showBoardList.php
@@ -3,15 +3,14 @@
 	前端 to 後端:
 	let cmd = {};
     cmd["act"] = "showBoardList";
-    cmd["account"]="00757033";
+    
 	
     後端 to 前端:
     dataDB = JSON.parse(data);
 	dataDB.status
 	若 status = true:
 		dataDB.info = ""
-        dataDB.data[i].boardName //有i筆boardName
-        dataDB.data[i].isModerator
+		dataDB.data[i] //有i筆boardName
 	否則
 		dataDB.errorCode = "Without any board now."
 		dataDB.data = ""
@@ -28,11 +27,7 @@
 			$arr=array();
 			for($i=0;$i<$resultCount;$i++){
                 $row = $result[$i];
-                $sql="SELECT `BoardName`,`Rule` FROM `Board` WHERE `UserID`=? AND `BoardName`=?";
-                $a=array(,$input['account'],$result[$i]);
-                $moderator = query($conn,$sql,$a,"SELECT");
-                $moderatorCount=count($moderator);
-                $arr[$i]=array("boardName"=>$row[0],"rule"=>$row[1],"isModerator"=>($moderatorCount > 0? 1:0) );
+                $arr[$i]=array("boardName"=>$row[0],"rule"=>$row[1]);
             }
             $rtn = successCode("Successfully show boardName list.",$arr);
         }
