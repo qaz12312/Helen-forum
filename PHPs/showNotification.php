d2bbec00d06dd2bfdb42b5fcd4baa8fc3e046f0d
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2021-01-01 22:26:54 +0800

    更正 - 將內容改回showNotice

--- PHPs/showNotification.php
+++ PHPs/showNotification.php
@@ -1,41 +1,42 @@
 <?php
-    /* 
-    前端 to 後端:
-    let cmd = {};
-    cmd["act"] = "sendNotification";
-    cmd["recipient"] = "收通知的account";
-    cmd["content"] = "Content"
+	/* 
+	前端 to 後端:
+	let cmd = {};
+	cmd["act"] = "showNotice";
+	cmd["account"] = "00757007";
 
     後端 to 前端:
     dataDB = JSON.parse(data);
-    dataDB.status
-    若 status = true:
-        dataDB.info = ""
-        dataDB.data = ""
-    否則
-        dataDB.errorCode = "Failed to send notification,Database exception."
-        dataDB.data = ""
-    */
-    function doSendNotification($input,$print=1){
+	dataDB.status
+	若 status = true:
+		dataDB.info = ""
+		dataDB.data[i] //有i筆通知
+		(
+			dataDB.data.time	// Time
+			dataDB.data.content	// Content
+		)
+	否則
+		dataDB.errorCode = "No notifications right now."
+		dataDB.data = ""
+	*/
+    function doShowNotification($input){
         global $conn;
-        $sql="INSERT INTO `Issue`(`UserID`,`Content`,`Type`) VALUES(?,?,?)";
-        $arr = array($input['recipient'], $input['content'],2);
-        query($conn,$sql,$arr,"INSERT");
-        
-        $sql="SELECT `UserID`,`Times`,`Content` FROM `Issue` WHERE `UserID`=? AND`Content`=? AND `Type`= ? ";
-        $arr = array($input['recipient'], $input['content'],2);
+        $sql="SELECT `Times`,`Content` FROM `Issue` WHERE `UserID`=? AND `Type`=2 order by `Times`DESC ";
+        $arr = array($input['account']);
         $result = query($conn,$sql,$arr,"SELECT");
         $resultCount = count($result);
         if($resultCount <= 0){
-            errorCode("Failed to send notification,Database exception.");
+            $rtn = successCode("No notifications right now.");
         }
         else{
-			if($print){
-				$rtn = successCode("Successfully send the notice.");
-				echo json_encode($rtn);
+            $arr=array();
+            // foreach($result as $row){
+            for($i=0;$i<$resultCount;$i++){
+                $row = $result[$i];
+                $arr[$i]=array("time"=>$row[0],"content"=>$row[1]);
             }
-            else
-                return true;
+            $rtn = successCode("Successfully show person's notices.",$arr);
         }
+        echo json_encode($rtn);
     }
 ?>
