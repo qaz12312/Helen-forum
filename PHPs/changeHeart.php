a0e5f7c0435acb0a0d47b4972415011dec4d6b63
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2021-01-02 15:36:50 +0800

    修改 - token

--- PHPs/changeHeart.php
+++ PHPs/changeHeart.php
@@ -5,7 +5,6 @@
 	cmd["act"] = "heart";
 	cmd["account"] = "00757003"; //cmd["token"]
 	cmd["articleID"] = 1;
-
 	後端 to 前端:
 	dataDB = JSON.parse(data);
 	dataDB.status
@@ -19,32 +18,29 @@
 	function doAddDeleteHeart($input)
 	{
 		global $conn;
-		// $token =$input['token'];
+		// $token = $input['token'];
         // if(!isset($_SESSION[$token])){
 		// 	errorCode("token doesn't exist.");
-        // }else{
-		// 	$userInfo = $_SESSION[$token];
-			$sql = "SELECT `UserID`,`ArticleID` FROM `FollowHeart` WHERE `UserID`=? AND `ArticleID`=?";
-			// $arr = array($userInfo['account'],$input['articleID']);
-			$arr = array($input['account'],$input['articleID']);
-			$result = query($conn,$sql,$arr,"SELECT");
-			$resultCount = count($result);
-			if ($resultCount <= 0) {//新增HEART
-				$sql = "INSERT INTO `FollowHeart`(`ArticleID`,`UserID`) VALUES(?,?)";
-				// $arr = array($input['articleID'],$userInfo['account']);
-				$arr = array($input['articleID'],$input['account']);
-				query($conn,$sql,$arr,"INSERT");
-				// writeRecord($userInfo["account"],$userInfo["log"],"add heart for articleID:".$input['articleID']);
-				$rtn = successCode("Successfully added this heart.");
-			} else {//DELETE HEART
-				$sql = "DELETE FROM `FollowHeart` WHERE `UserID`=? AND `ArticleID`=?";
-				// $arr = array($userInfo['account'],$input['articleID']);
-				$arr = array($input['account'],$input['articleID']);
-				query($conn,$sql,$arr,"DELETE");
-				//writeRecord($userInfo["account"],$userInfo["log"],"cancel heart for articleID:".$input['articleID']);
-				$rtn = successCode("Successfully deleted this heart.");
-			}
-			echo json_encode($rtn);
-		// }
+        // }
+        // $userInfo = $_SESSION[$token];
+        // $user = $userInfo['account'];
+        $user = $input['account'];
+        $sql = "SELECT EXISTS(SELECT 1 FROM `FollowHeart` WHERE `UserID`=? AND `ArticleID`=? LIMIT 1";
+        $arr = array($user,$input['articleID']);
+        $result = query($conn,$sql,$arr,"SELECT");
+        if ($result[0][0] == 0) {//新增HEART
+            $sql = "INSERT INTO `FollowHeart`(`ArticleID`,`UserID`) VALUES(?,?)";
+            $arr = array($input['articleID'],$user);
+            query($conn,$sql,$arr,"INSERT");
+            // writeRecord($user,$userInfo["log"],"add heart for articleID:".$input['articleID']);
+            $rtn = successCode("Successfully added this heart.");
+        } else {//DELETE HEART
+            $sql = "DELETE FROM `FollowHeart` WHERE `UserID`=? AND `ArticleID`=?";
+            $arr = array($user,$input['articleID']);
+            query($conn,$sql,$arr,"DELETE");
+            //writeRecord($user,$userInfo["log"],"cancel heart for articleID:".$input['articleID']);
+            $rtn = successCode("Successfully deleted this heart.");
+        }
+        echo json_encode($rtn);
 	}
 ?>
