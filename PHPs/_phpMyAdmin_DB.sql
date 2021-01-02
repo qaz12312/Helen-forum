b505c3494a246e2cba1fe8a296a464a295aeb999
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2021-01-02 20:01:23 +0800

    Update _phpMyAdmin_DB.sql

--- PHPs/_phpMyAdmin_DB.sql
+++ PHPs/_phpMyAdmin_DB.sql
@@ -1,15 +1,15 @@
 CREATE TABLE Users (
-	UserID varchar(255) NOT NULL,
+	UserID varchar(101) NOT NULL,
 	Password varchar(12) NOT NULL,
 	IsAdmin boolean default false,
 	Color varchar(10) NOT NULL,
-	Nickname varchar(50) ,
+	Nickname varchar(21) ,
 	PRIMARY KEY (UserID)
 ) CHARSET=utf8mb4 ;
 
 CREATE TABLE Board (
 	BoardName varchar(255) NOT NULL UNIQUE,
-	UserID varchar(255) NOT NULL,
+	UserID varchar(101) NOT NULL,
 	Rule mediumtext ,
 	TopArticleID bigint(255) ,
 	PRIMARY KEY (BoardName),
@@ -18,42 +18,49 @@ CREATE TABLE Board (
 
 CREATE TABLE Article (
 	ArticleID bigint(255) NOT NULL AUTO_INCREMENT,
-	AuthorID varchar(255) NOT NULL,
+	AuthorID varchar(101) NOT NULL,
 	Title varchar(255) NOT NULL,
-	Content longtext ,
+	Content text ,
 	Image longblob ,
 	HashTag varchar(255) ,
 	Times datetime DEFAULT CURRENT_TIMESTAMP,
 	BlockName varchar(255) NOT NULL,
-    PRIMARY KEY (ArticleID),
-    FOREIGN KEY (AuthorID) REFERENCES Users (UserID),
-    FOREIGN KEY (BlockName) REFERENCES Board (BoardName) ON UPDATE CASCADE  ON DELETE CASCADE
+PRIMARY KEY (ArticleID),
+FOREIGN KEY (AuthorID) REFERENCES Users (UserID),
+FOREIGN KEY (BlockName) REFERENCES Board (BoardName) ON UPDATE CASCADE  ON DELETE CASCADE
 ) CHARSET=utf8mb4 ;
 
 CREATE TABLE Report (
-	UserID varchar(255) NOT NULL,
+	UserID varchar(101) NOT NULL,
 	ArticleID bigint(255) NOT NULL,
-	Reason text NOT NULL,
+	Reason varchar(255) NOT NULL,
 	Times datetime DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (UserID, ArticleID),
 	FOREIGN KEY (ArticleID) REFERENCES Article (ArticleID) ON DELETE CASCADE
 ) CHARSET=utf8mb4 ;
 
 CREATE TABLE Comments (
-	AuthorID varchar(255) NOT NULL,
+	AuthorID varchar(101) NOT NULL,
 	Content mediumtext NOT NULL,
 	ArticleID bigint(255) NOT NULL,
 	Times datetime DEFAULT CURRENT_TIMESTAMP,
 	Floor int(255) NOT NULL,
 	#TagFloor int(255),
 	PRIMARY KEY (ArticleID, Floor),
-    FOREIGN KEY (AuthorID) REFERENCES Users (UserID),
-    FOREIGN KEY (ArticleID) REFERENCES Article (ArticleID)  ON DELETE CASCADE
+FOREIGN KEY (AuthorID) REFERENCES Users (UserID),
+FOREIGN KEY (ArticleID) REFERENCES Article (ArticleID)  ON DELETE CASCADE
+) CHARSET=utf8mb4 ;
+
+CREATE TABLE KeepDir (
+	UserID varchar(101) NOT NULL,
+	DirName varchar(255) NOT NULL,
+	PRIMARY KEY (UserID, DirName),
+	FOREIGN KEY (UserID) REFERENCES Users (UserID)
 ) CHARSET=utf8mb4 ;
 
 CREATE TABLE FollowHeart (
 	ArticleID bigint(255) NOT NULL,
-	UserID varchar(255) NOT NULL,
+	UserID varchar(101) NOT NULL,
 	PRIMARY KEY (ArticleID, UserID),
 	FOREIGN KEY (ArticleID) REFERENCES Article (ArticleID)  ON DELETE CASCADE,
 	FOREIGN KEY (UserID) REFERENCES Users (UserID)
@@ -61,62 +68,52 @@ CREATE TABLE FollowHeart (
 
 CREATE TABLE FollowKeep (
 	ArticleID bigint(255) NOT NULL,
-	UserID varchar(255) NOT NULL,
+	UserID varchar(101) NOT NULL,
 	DirName varchar(255) NOT NULL,
 	AddTime datetime DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (ArticleID, UserID),
-	FOREIGN KEY (ArticleID) REFERENCES Article (ArticleID) ON DELETE CASCADE,
+	FOREIGN KEY (ArticleID) REFERENCES Article (ArticleID)   ON DELETE CASCADE,
 	FOREIGN KEY (UserID, DirName) REFERENCES KeepDir (UserID, DirName)	ON UPDATE CASCADE ON DELETE CASCADE
 ) CHARSET=utf8mb4 ;
 
-CREATE TABLE Notice (
-	UserID varchar(255) NOT NULL,
+CREATE TABLE Issue (
+	UserID varchar(101) NOT NULL,
 	Times datetime DEFAULT CURRENT_TIMESTAMP,
 	Content varchar(255) NOT NULL,
-	PRIMARY KEY (UserID, Times, Content),
+	Type int(2) NOT NULL,
+	PRIMARY KEY (UserID, Content),
 	FOREIGN KEY (UserID) REFERENCES Users (UserID)
 ) CHARSET=utf8mb4 ;
 
-CREATE TABLE KeepDir (
-	UserID varchar(255) NOT NULL,
-	DirName varchar(255) NOT NULL,
-	PRIMARY KEY (UserID, DirName),
-	FOREIGN KEY (UserID) REFERENCES Users (UserID)
-) CHARSET=utf8mb4 ;
-
-CREATE TABLE Issue (
-	UserID varchar(255) NOT NULL,
-    Content longtext ,
-    Times datetime DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (UserID, Times),
-    FOREIGN KEY (UserID) REFERENCES Users (UserID)
-) CHARSET=utf8mb4 ;
-
-CREATE TABLE Calendar (
-	UserID varchar(255) NOT NULL,
+CREATE TABLE Calendars (
+	ID bigint(255) NOT NULL AUTO_INCREMENT,
+	UserID varchar(101) NOT NULL,
 	Title varchar(255) NOT NULL,
     Start varchar(101) NOT NULL,
     END varchar(101) NOT NULL,
     Text text,
     IsValid boolean default false,
-    PRIMARY KEY (UserID, Title),
+    PRIMARY KEY (ID),
     FOREIGN KEY (UserID) REFERENCES Users (UserID)
 )	CHARSET=utf8mb4 ;
 
 
 # 總愛心數
-CREATE VIEW HomeHeart (`BoardName`,`ArticleID`,`Title`,`Content`,`cntHeart`,`Times`, `AuthorID`) AS 
-SELECT `BoardName`,Article.ArticleID,`Title`,`Content`,COUNT(FollowHeart.UserID),`Times`, `AuthorID`
+DROP VIEW IF EXISTS HomeHeart;
+CREATE VIEW HomeHeart (`BoardName`,`ArticleID`,`Title`,`Content`,`cntHeart`,`Times`, `AuthorID`, `Hashtag`) AS 
+SELECT `BoardName`,Article.ArticleID,`Title`,`Content`,COUNT(FollowHeart.UserID),`Times`, `AuthorID`, `Hashtag`
 FROM `Article` JOIN `Board` ON Article.BlockName = Board.BoardName LEFT JOIN `FollowHeart` ON Article.ArticleID = FollowHeart.ArticleID
 GROUP BY `ArticleID` ;
 
 # 總收藏數
-CREATE VIEW HomeKeep (`BoardName`,`ArticleID`,`Title`,`Content`,`cntKeep`,`Times`, `AuthorID`) AS 
-SELECT `BoardName`,Article.ArticleID,`Title`,`Content`,COUNT(FollowKeep.UserID),`Times`, `AuthorID`
+DROP VIEW IF EXISTS HomeKeep;
+CREATE VIEW HomeKeep (`BoardName`,`ArticleID`,`Title`,`Content`,`cntKeep`,`Times`, `AuthorID`, `Hashtag`) AS 
+SELECT `BoardName`,Article.ArticleID,`Title`,`Content`,COUNT(FollowKeep.UserID),`Times`, `AuthorID`, `Hashtag`
 FROM `Article` JOIN `Board` ON Article.BlockName = Board.BoardName LEFT JOIN `FollowKeep` ON Article.ArticleID = FollowKeep.ArticleID
 GROUP BY `ArticleID` ;
 
 # 總留言數
+DROP  VIEW IF EXISTS HomeComment;
 CREATE VIEW HomeComment (`ArticleID`, `cntComment`) AS
 SELECT `ArticleID`, COUNT(Comments.Content)
 FROM `Article`  LEFT JOIN `Comments` USING (ArticleID)  
