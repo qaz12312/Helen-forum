5d1eb289471b2dfa97574048d996fe9a1fda029c
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2021-01-02 15:42:32 +0800

    修改 - token + 加註解

--- PHPs/changeKeep.php
+++ PHPs/changeKeep.php
@@ -6,7 +6,6 @@
 	cmd["account"] = "00757003"; //cmd["token"]
 	cmd["articleID"] = 2;
 	cmd["dirName"] = "旅遊景點";(如果是收藏文章才要)
-
 	後端 to 前端:
 	dataDB = JSON.parse(data);
 	dataDB.status
@@ -23,40 +22,36 @@
 		// $token =$input['token'];
         // if(!isset($_SESSION[$token])){
 		// 	errorCode("token doesn't exist.");
-        // }else{
-		// 	$userInfo = $_SESSION[$token];
-			$sql = "SELECT `UserID`,`ArticleID` FROM `FollowKeep` WHERE `UserID`=? AND `ArticleID`=?";
-			// $arr = array($userInfo['account'],$input['articleID']);
-			$arr = array($input['account'],$input['articleID']);
-			$result = query($conn,$sql,$arr,"SELECT");
-			$resultCount = count($result);
-			if ($resultCount <= 0) {	//收藏文章
-				$sql ="SELECT `UserID`,`DirName` FROM `KeepDir` WHERE `UserID`=? AND`DirName`=?";
-				//$arr = array($userInfo['account'], $input['dirName']);
-				$arr = array($input['account'], $input['dirName']);
-				$result = query($conn,$sql,$arr,"SELECT");
-				$resultCount = count($result);
-				if($resultCount <= 0){
-					errorCode("Keep without folder.");
-				}
-				else{
-					$sql = "INSERT INTO `FollowKeep`(`ArticleID`,`UserID`,`DirName`) VALUES(?,?,?)";
-					//$arr = array($input['articleID'], $userInfo['account'],$input['dirName'] );
-					$arr = array($input['articleID'],  $input['account'],$input['dirName'] );
-					query($conn,$sql,$arr,"INSERT");
-				// writeRecord($userInfo["account"],$userInfo["log"],"add articleID:".$input['articleID']."in dirName=".$input['dirName']);
-					$rtn = successCode("Successfully kept this keep the article.");
-				}
-			} 
-			else {	//收回收藏
-				$sql = "DELETE FROM `FollowKeep` WHERE `UserID`=? AND `ArticleID`=?";
-				//$arr = array($userInfo["account"], $input['articleID']);
-				$arr = array($input['account'], $input['articleID']);
-				query($conn,$sql,$arr,"DELETE");
-				// writeRecord($userInfo["account"],$userInfo["log"],"remove articleID:".$input['articleID']."from dirName=".$input['dirName']);
-				$rtn = successCode("Successfully deleted this article.");
-			}
-			echo json_encode($rtn);
-		// }	
+        // }
+        // $userInfo = $_SESSION[$token];
+        // $user = $userInfo['account'];
+
+        $user = $input['account'];
+        $sql = "SELECT EXISTS(SELECT 1 FROM `FollowKeep` WHERE `UserID`=? AND `ArticleID`=? LIMIT 1)"; //文章是否存在
+        $arr = array($user,$input['articleID']);
+        $result = query($conn,$sql,$arr,"SELECT");
+        if ($result[0][0] == 0) {	//收藏文章
+            $sql ="SELECT EXISTS(SELECT 1 FROM `KeepDir` WHERE `UserID`=? AND`DirName`=? LIMIT 1)";//收藏資料夾是否存在
+            $arr = array($user, $input['dirName']);
+            $result = query($conn,$sql,$arr,"SELECT");
+            if($result[0][0] == 0){
+                errorCode("Keep without folder.");
+            }
+            else{
+                $sql = "INSERT INTO `FollowKeep`(`ArticleID`,`UserID`,`DirName`) VALUES(?,?,?)";
+                $arr = array($input['articleID'],$user,$input['dirName'] );
+                query($conn,$sql,$arr,"INSERT");
+            // writeRecord($user,$userInfo["log"],"add articleID:".$input['articleID']."in dirName=".$input['dirName']);
+                $rtn = successCode("Successfully kept this keep the article.");
+            }
+        } 
+        else {	//收回收藏
+            $sql = "DELETE FROM `FollowKeep` WHERE `UserID`=? AND `ArticleID`=?";
+            $arr = array($user, $input['articleID']);
+            query($conn,$sql,$arr,"DELETE");
+            // writeRecord($user,$userInfo["log"],"remove articleID:".$input['articleID']."from dirName=".$input['dirName']);
+            $rtn = successCode("Successfully deleted this article.");
+        }
+        echo json_encode($rtn);	
 	}
 ?>
