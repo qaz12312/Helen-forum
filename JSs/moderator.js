9139989ccb3e69f3275ad22bf427e9aabca8b95b
ytchao0234 <nd68319@gmail.com>
2021-01-04 02:33:18 +0800

    fix moderator

--- JSs/moderator.js
+++ JSs/moderator.js
@@ -361,7 +361,7 @@ function moderatorInitial( resolve, reject )
                                         "<span class='glyphicon glyphicon-plus'></span>" +
                                     "</td>" +
                                     "<td>" +
-                                        "<div class='input-group input-group-lg' style='margin: 0;'>" +
+                                        "<div class='input-group input-group-lg' style='margin: 0 !important;'>" +
                                             "<input id='users' list='userList' value='' class='textInput'>" +
                                             "<datalist id='userList' class='textInput'>" +
                                                 userOptions +
