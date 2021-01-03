5331904eb658af2d818ac78a5c9895fcb4aba2c3
ytchao0234 <nd68319@gmail.com>
2021-01-04 02:28:58 +0800

    fix moderator

--- JSs/moderator.js
+++ JSs/moderator.js
@@ -360,8 +360,8 @@ function moderatorInitial( resolve, reject )
                                     "<td style='text-align: center;'>" +
                                         "<span class='glyphicon glyphicon-plus'></span>" +
                                     "</td>" +
-                                    "<td style='text-align: center;'>" +
-                                        "<div class='input-group input-group-lg' style='text-align: center;'>" +
+                                    "<td'>" +
+                                        "<div class='input-group input-group-lg' style=margin: 0;'>" +
                                             "<input id='users' list='userList' value='' class='textInput'>" +
                                             "<datalist id='userList' class='textInput'>" +
                                                 userOptions +
