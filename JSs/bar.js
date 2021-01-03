23d6e20fbb44cd68e678d34e6c63afc83b7836de
acs107102 <acs107102@gm.ntcu.edu.tw>
2021-01-03 17:31:17 +0800

    Update bar.js

--- JSs/bar.js
+++ JSs/bar.js
@@ -17,13 +17,22 @@ async function barInitial() {
     // 最左邊的 ham menu 初始化
     $("#menu").empty();
     let boards = sessionStorage.getItem("Helen-boards");
+	let userBoard = sessionStorage.getItem("User-boards");
     boards = JSON.parse(boards);
-
+	userBoard = JSON.parse(userBoard);
+	console.log(userBoard.find(element => element == boards[10]));
+	//console.log(boards.length);
     for (var i = 0; i < boards.length; i++) {
         var oneBoard = boards[i];
-        $("#menu").append("<a href=\"../HTMLs/sticky.html\"><li>" +
-            oneBoard +
-            "版</li></a>");
+		if (userBoard.find(element => element == oneBoard)) {
+			$("#menu").append("<a href=\"../HTMLs/sticky.html\"><li>" + oneBoard +
+                                "版 <a href=\"../HTMLs/report.html\" class= \"glyphicon glyphicon-alert\"></li></a>");
+		}
+		else {
+			$("#menu").append("<a href=\"../HTMLs/sticky.html\"><li>" +
+				oneBoard +
+				"版</li></a>");
+		}
     }
 
     if (userPermission >= 3) // admin 可新增看版
@@ -84,10 +93,13 @@ async function barInitial() {
         $("#userDD").append("<li class= \"glyphicon glyphicon-calendar\">"+
                             "<a href=\"../HTMLs/calendar.html\"> 行事曆</a></li>")
 
+		/*
         if(userPermission>= 2){ // 版主
             $("#userDD").append("<li class= \"glyphicon glyphicon-alert\">"+
                                 "<a href=\"../HTMLs/report.html\"> 檢舉區</a></li>")
         }
+		*/
+		
         if(userPermission>= 3){
             $("#userDD").append("<li class= \"glyphicon glyphicon-th-list\">"+
                                 "<a href=\"../HTMLs/manageBoard.html\"> 管理看版</a></li>")
@@ -192,6 +204,7 @@ function getUserInfo(resolve, reject) {
     }
     else {
         let cmd = {};
+		let userBoard = [];
         cmd["act"] = "showAuthority";
         cmd["account"] = sessionStorage.getItem("Helen-account");
         $.post("../index.php", cmd, function (dataDB) {
@@ -208,6 +221,13 @@ function getUserInfo(resolve, reject) {
                 // Test
                 //  userData= {permission: 2};
                 // Test End
+				console.log(userData.boardName[0].BoardName);
+				
+				for (var i = 0; i < userData.boardName.length; i++) {
+					userBoard.push(userData.boardName[i].BoardName);
+				}
+				console.log(userBoard);
+				sessionStorage.setItem("User-boards", JSON.stringify(userBoard));
                 userPermission = userData.permission;
                 delete userData.permission;
             }
@@ -238,7 +258,7 @@ function getBoards(resolve, reject) {
             if (boards.length == 0) {
                 console.log("沒有看版");
             }
-
+			console.log(dataDB);
             sessionStorage.setItem("Helen-boards", JSON.stringify(boards));
         }
         resolve(0);
