a1c93f45347d407d77ef0ba15cd00f75f9569057
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2021-01-03 20:38:29 +0800

    test notice

--- JSs/notice.js
+++ JSs/notice.js
@@ -5,7 +5,70 @@ $( document ).ready( async function()
 {
     barInitial();
     await new Promise( ( resolve, reject ) => initial( resolve, reject ) );
+    $(".tabContent button").on( "click", function(){
+        let articleIndex = $(".tabContent tr").index(this.closest("tr"));
+        if( $(this).text().trim() == "刪除"){
+            let cmd= {};
+            cmd["act"] = "clickNotice";
+            cmd["account"] = sessionStorage.getItem("Helen-account");
+            cmd["articleID"] = articles[articleIndex].articleID;
+            cmd["detail"] = articles[articleIndex].content;
 
+            $.post( "../index.php", cmd, function( dataDB ){
+                alert(dataDB);
+                console.log(dataDB);
+                dataDB = JSON.parse( dataDB );
+
+                swal({
+                    title: "確定要刪除此通知嗎？<br /><small>&lt;"
+                     + articles[ articleIndex ].content
+                      + "&gt;</small>",
+                    showCancelButton: true,
+                    confirmButtonText: "確定",
+                    cancelButtonText: "取消",
+                    animation: false
+    
+                    }).then(( result ) => {
+                        if ( result ) 
+                        {
+                            if( status == false )
+                            {
+                                swal({
+                                    title: "移除失敗<br /><small>&lt;"
+                                    
+                                      + "&gt;</small>",
+                                    type: "error",
+                                    
+                                    animation: false
+                                }, ( dismiss ) => {});
+                            }
+                            else
+                            {
+                                swal({
+                                    title: "已成功此通知！<br /><small>&lt;"
+                                     + articles[ articleIndex ].content
+                                      + "&gt;</small>",
+                                    type: "success",
+                                }).then(( result ) => {}, ( dismiss ) => {});
+                                $(this).closest( "tr" ).remove();
+                                articles.splice( articleIndex, 1 );
+    
+                                if( articles.length == 0 )
+                                {
+                    
+                                    let emptyMessage = "<tr>" + 
+                                                            "<td colspan='2'>沒有通知喔呦！</td>" +
+                                                        "</tr>";
+                                    $( ".tabContent tbody" ).append( emptyMessage );
+                                }
+                            }
+                        }
+                    }, function( dismiss ) {
+                        if ( dismiss === 'cancel' );
+                });
+            });
+        }
+    });
     
 });
 
@@ -58,10 +121,14 @@ async function initial( res, rej )
                 console.log(articles[i].time)
                 
                 let oneRow = "<tr>" +
-                
-                                "<td rowspan='2'>" + articles[i].time  + "</td>" +
-                                "<td rowspan='6'>" + articles[i].content  + "</td>" 
-                                ;
+                                "<td>" + articles[i].content  + "</td>" +
+                                "<td>" + articles[i].time  + "</td>" +
+                                "<button type='button' class='btn btn-default'>" +
+                                        "<span class='glyphicon glyphicon-remove'></span> 刪除" +
+                                    "</button>" +
+                                "</td>" +
+                            "</tr>";
+                                
 
                 content.append( oneRow );
             }
