a918f04cc48555d18c2da9c0702bd305c20276a5
qaz12312 <49808673+qaz12312@users.noreply.github.com>
2021-01-04 13:10:22 +0800

    Update sortInBoard.php

--- PHPs/sortInBoard.php
+++ PHPs/sortInBoard.php
@@ -35,7 +35,7 @@
         $boardInfo = query($conn,$sql,$arr,"SELECT");
         $resultCount = count($boardInfo);
         if ($resultCount <= 0) {
-            $rtn = successCode("Failed.", array("articleList"=>array(),"topArticleID"=>$boardInfo[0]['TopArticleID'],"rule"=>$boardInfo[0]['Rule']));
+            $rtn = successCode("Failed.", array());
         } 
 
         if ($input['sort'] == "time" || $input['sort'] == "hot" || $input['sort'] == "collect" || $input['sort'] == "comment") {
