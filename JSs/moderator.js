7213a58fca572fe5917a8b0fd35d5f99674a95b9
ytchao0234 <nd68319@gmail.com>
2021-01-04 20:43:21 +0800

    test delete moderator

--- JSs/moderator.js
+++ JSs/moderator.js
@@ -397,13 +397,13 @@ function moderatorInitial( resolve, reject )
                                 "<td>";
                 }
         
-                selectBlock = "<div class='input-group input-group-lg mt-3'>" +
-                                    "<select class='form-control' style='background-color: brown; color: white;'>" +
+                selectBlock = "<div class='input-group input-group-lg mt-3 row'>" +
+                                    "<select class='form-control col-10' style='background-color: brown; color: white;'>" +
                                         "<option value='" + dataDB.data[i].boardName + "' selected>" + dataDB.data[i].boardName + "版</option>" +
                                         validOptions + 
                                     "</select>" +
 
-                                    "<div class='input-group-append'>" +
+                                    "<div class='input-group-append col-2'>" +
                                         "<button class='btn btn-danger fa fa-minus' type='button' style='width: 40px !important;'>" +
                                         "</button>" +
                                     "</div>" +
