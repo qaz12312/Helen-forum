ff6fa1955e3a9f9af3469ca9c29bffdff72ba247
PollyYang <watermoo22@gmail.com>
2021-01-05 17:11:45 +0800

    新增 - isAuthor到前端

--- PHPs/showArticleComment.php
+++ PHPs/showArticleComment.php
@@ -21,7 +21,7 @@
             dataDB.data.comment[i].isOwn      //第i筆留言是我留的
         ) 
         dataDB.data.boardName   //文章的所屬版
-	dataDB.data.title   //文章的標題
+	    dataDB.data.title   //文章的標題
         dataDB.data.content //文章的內容
         dataDB.data.like    //文章的總愛心數
         dataDB.data.keep    //文章的總收藏數
@@ -31,6 +31,7 @@
 		dataDB.data.hashTag
         dataDB.data.authorNickname 
         dataDB.data.authorColor    
+        dataDB.data.isAuthor // user是否為發文者(0/1)
     否則
         dataDB.errorCode = "Article doesn't exit."
         dataDB.data = ""
@@ -39,20 +40,20 @@
         global $conn;
         $articleID = $input['articleID'];
         $sql="SELECT `AuthorID`,`Title`,`Content`,`cntHeart`,`cntKeep`,`Times`,`Hashtag`,`boardName`,`Image` FROM HomeHeart NATURAL JOIN HomeKeep WHERE `ArticleID`=?"; //文章 相關資訊
-        $result = query($conn,$sql,array($articleID),"SELECT");
-        $resultCount = count($result);
+        $articleInfo = query($conn,$sql,array($articleID),"SELECT");
+        $resultCount = count($articleInfo);
         if($resultCount <= 0){
             errorCode("Article doesn't exit.");
         }
         else{
             $sql="SELECT `Nickname`,`Color` FROM `Users` WHERE `UserID`=?"; //作者 相關資訊
-            $resultAuthor = query($conn,$sql,array($result[0][0]),"SELECT");
+            $resultAuthor = query($conn,$sql,array($articleInfo[0][0]),"SELECT");
             $resultCount = count($resultAuthor);
             if($resultCount <= 0){
                 errorCode("AuthorID doesn't exit.");
             }
             else{
-                $hashTag = json_decode($result[0][6]);
+                $hashTag = json_decode($articleInfo[0][6]);
                 // if(isset($input['token'])){// 非訪客
                 //     $token =$input['token'];
                 //     if(!isset($_SESSION[$token])){
@@ -62,7 +63,7 @@
                 //     $user = $userInfo['account'];
                 // }
                 if(isset($input['account'])){
-                    $user=$input['account'];
+                    $user = $input['account'];
                     $sql = "SELECT EXISTS(SELECT 1 FROM `FollowHeart` WHERE `ArticleID`=? AND`UserID`=? LIMIT 1)";
                     $heart = query($conn, $sql, array($articleID, $user), "SELECT");
                     $hasLike = $heart[0][0];
@@ -70,11 +71,15 @@
                     $sql = "SELECT EXISTS(SELECT 1 FROM `FollowKeep` WHERE `ArticleID`=? AND`UserID`=? LIMIT 1)";
                     $keep = query($conn, $sql, array($articleID, $user), "SELECT");
                     $hasKeep = $keep[0][0];
+
+                    $isAuthor = ($input['account']==$articleInfo[0][0]) ? 1 : 0 ;
+
                 }else{
                     $hasLike = "" ;
                     $hasKeep = "" ;
+                    $isAuthor = 0 ;
                 }
-                $arr = array("boardName"=>$result[0][7],"title"=>$result[0][1],"content"=>$result[0][2],"like"=>$result[0][3],"keep"=>$result[0][4],"time"=>$result[0][5],"image"=>$result[0][8],"hashTag"=>$hashTag,"authorNickName"=>$resultAuthor[0][0] ,"authorColor"=>$resultAuthor[0][1], "hasLike" => $hasLike, "hasKeep" =>$hasKeep);
+                $arr = array("isAuthor"=>$isAuthor,"boardName"=>$articleInfo[0][7],"title"=>$articleInfo[0][1],"content"=>$articleInfo[0][2],"like"=>$articleInfo[0][3],"keep"=>$articleInfo[0][4],"time"=>$articleInfo[0][5],"image"=>$articleInfo[0][8],"hashTag"=>$hashTag,"authorNickName"=>$resultAuthor[0][0] ,"authorColor"=>$resultAuthor[0][1], "hasLike" => $hasLike, "hasKeep" =>$hasKeep);
                 
                 $sql ="SELECT `UserID`, `Nickname`, `Color`,`Content`,`Floor`,`Times` FROM Comments JOIN Users ON Users.UserID=Comments.AuthorID WHERE `ArticleID`=? order by Floor ASC " ; //留言 相關資訊 
                 $comment = query($conn,$sql,array($articleID),"SELECT");
