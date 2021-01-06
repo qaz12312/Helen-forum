4aad521f2124ba61e164ccf97841e7812db6790b
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2021-01-06 23:13:43 +0800

    Update post.js

--- JSs/post.js
+++ JSs/post.js
@@ -472,7 +472,7 @@ $(".content p").on("click", ".hashTagSearch", function () {
     var hashtags = [];
     var button = [];
     let searchData = {};
-    button.push(button);
+    button.push(hashTagStr);
     searchData["content"] = contents;
     searchData["hashtag"] = hashtags;
     searchData["button"] = button;
