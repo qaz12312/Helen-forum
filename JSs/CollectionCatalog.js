6ac96ad5f67e35e5cb216527b8657b5c16cdd67a
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2021-01-11 20:31:22 +0800

    Update CollectionCatalog.js

--- JSs/CollectionCatalog.js
+++ JSs/CollectionCatalog.js
@@ -25,13 +25,7 @@ $( document ).ready(async function()
             animation: false
                 
             }).then(( result ) => {
-                let cmd = {};
-                cmd["act"] = "editDir";
-                cmd["account"] = sessionStorage.getItem("Helen-account");
-                cmd["old"] =$(this).parents('.Page').find("span").text();
-                cmd["new"]=result
-                $.post( "../index.php", cmd, function( dataDB ){
-                dataDB = JSON.parse( dataDB );
+                
                     if(values.includes(result)==true)
                     {
     
@@ -42,16 +36,24 @@ $( document ).ready(async function()
                         }).then(( result ) => {location.reload()}, ( dismiss ) => {location.reload()});
                         
                     }
-                    else if(result==""){
+                    else if($(this).parents('.Page').find("span").text()=="")
+                    {
                         swal({
                             title: "收藏名稱不得為空<br />",
                             type: "error",
                             animation: false
-                        }).then(( result ) => {location.reload();}, ( dismiss ) => {location.reload()});
+                        }).then(( result ) => {location.reload()}, ( dismiss ) => {location.reload()});
                         
                     }
                     else
                     {
+                        let cmd = {};
+                        cmd["act"] = "editDir";
+                        cmd["account"] = sessionStorage.getItem("Helen-account");
+                        cmd["old"] =$(this).parents('.Page').find("span").text();
+                        cmd["new"]=result
+                        $.post( "../index.php", cmd, function( dataDB ){
+                        dataDB = JSON.parse( dataDB );
                         
                         if( dataDB.status == false )
                         {
@@ -92,8 +94,9 @@ $( document ).ready(async function()
                             });
 
                         }
+                     });
                     }
-                });
+               
             
             }, ( dismiss ) => {} );           
             
