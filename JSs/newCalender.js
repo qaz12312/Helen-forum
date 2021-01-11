09e8a33c3c83a6c526429451b08c2a2a86bcb430
Popcorn0924-0924 <66465463+Popcorn0924-0924@users.noreply.github.com>
2021-01-11 23:16:07 +0800

    Update newCalender.js

--- JSs/newCalender.js
+++ JSs/newCalender.js
@@ -51,7 +51,7 @@ $("#newActivity").on("click", function(){
         }).then(( result ) => {}, ( dismiss ) => {});
         return;
     }
-    if(contentStr.length< 10){
+    if(contentStr.length< 1){
         //console.log("(Content) Too shor.")
         swal({
             title: "內容太少嘍！",
