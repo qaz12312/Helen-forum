fae17826f4077d5720950a8c064303f64c46db13
00757033 <lspss95270@gmail.com>
2021-01-12 13:04:23 +0800

    Update post.js

--- JSs/post.js
+++ JSs/post.js
@@ -76,7 +76,7 @@ async function initial(res, rej){
                 contentStr+= "<img src= \""+ article.image+ "\" style='width: 45%;height: 45%;'>";
             
             if(article.video)
-                contentStr+= "<iframe   width='500' height='500' src= \""+ article.video+ "\"></iframe>";
+                contentStr+= "<iframe   width='680' height='500' src= \""+ article.video+ "\"></iframe>";
             if(article.hashTag){
                 contentStr+= "<br/>";
                 for(var h= 0; h< article.hashTag.length; h++){ // 加上hashtags
